a4278aa6b5dae00e748217a9388533fb7e3c4012
unknown <sakarim9124@gmail.com>
2023-10-05 08:29:17 +0530

    updated

---
+++ react_interview/boilerplate/react_helper/src/App.js
@@ -0,0 +1,23 @@
+import React from "react";
+
+//Patterns
+import RenderProps from "./components/patterns/renderProps";
+
+//machineCoding
+import Todo from "./components/machineCoding/todo";
+
+const App = () => {
+  return (
+    <div>
+      <h1>App.js</h1>
+
+      {/* Patterns */}
+      {/* <RenderProps /> */}
+
+      {/* machine coding */}
+      <Todo />
+    </div>
+  );
+};
+
+export default App;
