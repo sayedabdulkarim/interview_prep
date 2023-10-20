48d037fc3b3c3f1601072f246a152e41eb3654d4
unknown <sakarim9124@gmail.com>
2023-10-20 16:54:13 +0530

    updated

--- serviceWorker/react_implementation/src/components/Users.js
+++ serviceWorker/react_implementation/src/components/Users.js
@@ -1,13 +1,41 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import Table from "react-bootstrap/Table";
 
 const Users = () => {
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then((res) => res.json())
+      .then((res) => setData(res))
+      .catch((err) => console.log(err, " errr"));
+  }, []);
+
   return (
     <div>
-      <h1>Users</h1>
-      <h1>Users</h1>
-      <h1>Users</h1>
-      <h1>Users</h1>
-      <h1>Users</h1>
+      <Table responsive>
+        <thead>
+          <tr>
+            <th>ID</th>
+            <th>Name</th>
+            <th>UserName</th>
+            <th>Email</th>
+          </tr>
+        </thead>
+        <tbody>
+          {[...data]?.map((item) => {
+            const { id, name, username, email } = item;
+            return (
+              <tr key={id}>
+                <td>{id}</td>
+                <td>{name}</td>
+                <td>{username}</td>
+                <td>{email}</td>
+              </tr>
+            );
+          })}
+        </tbody>
+      </Table>
     </div>
   );
 };
