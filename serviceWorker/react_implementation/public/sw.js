cf5cceb0a92577f70c1519021694b597a94b4418
unknown <sakarim9124@gmail.com>
2023-10-20 19:30:00 +0530

    updated

--- serviceWorker/react_implementation/public/sw.js
+++ serviceWorker/react_implementation/public/sw.js
@@ -23,23 +23,50 @@ self.addEventListener("install", (event) => {
 
 // Listen for fetch events
 // eslint-disable-next-line no-restricted-globals
+// self.addEventListener("fetch", (event) => {
+//   event.respondWith(
+//     // Check the cache for a cached version of the user's request
+//     caches.match(event.request).then((response) => {
+//       if (response) {
+//         // If there is a cached version, return it
+//         return response;
+//       }
+
+//       // If there isn't, fetch from the network
+//       return fetch(event.request).then((fetchResponse) => {
+//         // And cache the new fetched response
+//         return caches.open(cachedData).then((cache) => {
+//           cache.put(event.request, fetchResponse.clone());
+//           return fetchResponse;
+//         });
+//       });
+//     })
+//   );
+// });
+
+// eslint-disable-next-line no-restricted-globals
 self.addEventListener("fetch", (event) => {
   event.respondWith(
-    // Check the cache for a cached version of the user's request
-    caches.match(event.request).then((response) => {
-      if (response) {
-        // If there is a cached version, return it
-        return response;
-      }
-
-      // If there isn't, fetch from the network
-      return fetch(event.request).then((fetchResponse) => {
-        // And cache the new fetched response
-        return caches.open(cachedData).then((cache) => {
-          cache.put(event.request, fetchResponse.clone());
-          return fetchResponse;
-        });
-      });
-    })
+    // Check if the browser is online
+    navigator.onLine
+      ? // If online, try fetching from the network
+        fetch(event.request)
+          .then(async (fetchResponse) => {
+            // And cache the new fetched response
+            const cache = await caches.open(cachedData);
+            cache.put(event.request, fetchResponse.clone());
+            return fetchResponse;
+          })
+          .catch((error) => {
+            // If fetching from the network fails, try to serve from cache
+            return caches.match(event.request);
+          })
+      : // If offline, try to serve from cache
+        caches.match(event.request).then((response) => {
+          return (
+            response ||
+            new Response("You are offline, and this data is not cached.")
+          );
+        })
   );
 });
