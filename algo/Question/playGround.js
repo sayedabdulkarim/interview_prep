7990ac17ca6ce202ca8746c65ec488a8dc6e5e98
unknown <sakarim9124@gmail.com>
2023-10-22 12:56:29 +0530

    updated

--- algo/Question/playGround.js
+++ algo/Question/playGround.js
@@ -1,42 +1,19 @@
-function fibonacci(n) {
-  if (n === 0) return 0;
-  if (n === 1) return 1;
-  return fibonacci(n - 1) + fibonacci(n - 2);
-}
+function bubbleSort(arr) {
+  let isSorted = false;
 
-// console.log(fibonacci(7));
+  while (!isSorted) {
+    isSorted = true;
 
-// 0, 1, 1, 2, 3, 5, 8, 13;
-
-function countVowels(str) {
-  if (str.length == 0) return 0;
-  str = str.toLowerCase();
-  const vowels = ["a", "e", "i", "o", "u"];
-  let count = 0;
-  let lastElem = str[str.length - 1];
-
-  if (vowels.includes(lastElem)) {
-    count++;
+    for (let i = 0; i < arr.length; i++) {
+      if (arr[i] < arr[i + 1]) {
+        const temp = arr[i];
+        arr[i] = arr[i + 1];
+        arr[i + 1] = temp;
+        isSorted = false;
+      }
+    }
   }
-  str = str.slice(0, str.length - 1);
-  return count + countVowels(str);
+  return arr;
 }
 
-// function countVowels(str) {
-//   if (str.length == 0) return 0; // Base case: return 0 when string is empty
-//   str = str.toLowerCase();
-//   const vowels = ["a", "e", "i", "o", "u"];
-//   let count = 0;
-//   let lastElem = str[str.length - 1];
-
-//   if (vowels.includes(lastElem)) {
-//     count++;
-//   }
-
-//   // Accumulate counts by adding the current count to the recursive call's result
-//   return count + countVowels(str.slice(0, str.length - 1));
-// }
-
-console.log(countVowels("Hello, World!")); // Output: 3
-
-// console.log(countVowels("Hello, World!")); // Output: 3
+console.log(bubbleSort([64, 34, 25, 12, 22, 11, 90])); // Output: [11, 12, 22, 25, 34, 64, 90]
