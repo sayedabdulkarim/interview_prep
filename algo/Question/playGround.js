be9e5b7d77024373a7ed598760b0f4867a7a59e9
unknown <sakarim9124@gmail.com>
2023-10-22 20:46:49 +0530

    updated

--- algo/Question/playGround.js
+++ algo/Question/playGround.js
@@ -1,19 +1,2 @@
-function bubbleSort(arr) {
-  let isSorted = false;
-
-  while (!isSorted) {
-    isSorted = true;
-
-    for (let i = 0; i < arr.length; i++) {
-      if (arr[i] < arr[i + 1]) {
-        const temp = arr[i];
-        arr[i] = arr[i + 1];
-        arr[i + 1] = temp;
-        isSorted = false;
-      }
-    }
-  }
-  return arr;
-}
-
-console.log(bubbleSort([64, 34, 25, 12, 22, 11, 90])); // Output: [11, 12, 22, 25, 34, 64, 90]
+var obj = new Object({ name: "sdfg" });
+console.log(obj.name);
