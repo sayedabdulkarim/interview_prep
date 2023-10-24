f2ff5fe32eda38d1700867882c7c4ad92ed15e5d
unknown <sakarim9124@gmail.com>
2023-10-24 12:16:07 +0530

    updated

--- algo/Question/playGround.js
+++ algo/Question/playGround.js
@@ -1,58 +1,14 @@
-function removeDup(arr) {
-  if (!arr.length) return [];
-
-  arr.sort((a, b) => {
-    if (a < b) return -1;
-    if (a > b) return 1;
-    return 0;
-  });
-
-  let count = 0;
-
-  for (let i = 0; i < arr.length; i++) {
-    if (arr[count] !== arr[i]) {
-      count++;
-      arr[count] = arr[i];
+function currying(...args) {
+  const add = (...newArgs) => {
+    if (newArgs.length === 0) {
+      return args.reduce((a, b) => a + b, 0);
+    } else {
+      return currying(...args, ...newArgs);
     }
-  }
+  };
 
-  arr.length = count + 1;
-
-  return arr;
+  return add;
 }
 
-// removeDup();
-// console.log(removeDup([1, 3, 2, 4, 5, 1, 3]));
-
-function twoSum(arr, t) {
-  const obj = {};
-  for (let i = 0; i < arr.length; i++) {
-    const temp = t - arr[i];
-    if (obj[temp] !== undefined) {
-      return [obj[temp], i];
-    }
-
-    obj[arr[i]] = i;
-  }
-}
-
-// console.log(twoSum([1, 3, 2, 4, 5, 1, 3], 9));
-
-function twoSumSorted(arr, t) {
-  let left = 0;
-  let right = arr.length - 1;
-
-  while (left < right) {
-    const sum = arr[left] + arr[right];
-
-    if (t === sum) {
-      return [right, left];
-    } else if (left < right) {
-      left++;
-    } else right--;
-  }
-
-  return 0;
-}
-
-console.log(twoSumSorted([1, 3, 2, 4, 5, 8], 11));
+const result = currying(4)(5)(6)(7)();
+console.log(result); // Output: 22
