ce7fd6d0c81b1efddb8c342b1c5896228f24d22f
unknown <sakarim9124@gmail.com>
2023-10-25 00:06:28 +0530

    updated

--- algo/Question/playGround.js
+++ algo/Question/playGround.js
@@ -49,4 +49,64 @@ function twoSumSorted(arr, k) {
   return null;
 }
 
-console.log(twoSumSorted([1, 1, 2, 2, 3, 4, 5, 5], 2));
+// console.log(twoSumSorted([1, 1, 2, 2, 3, 4, 5, 5], 2));
+
+function maxSubArr(arr) {
+  if (!arr.length) return [];
+
+  let maxCurrent = arr[0];
+  let maxGlobal = arr[0];
+
+  for (let i = 1; i < arr.length; i++) {
+    maxCurrent = Math.max(arr[i], maxCurrent + arr[i]);
+    if (maxCurrent > maxGlobal) {
+      maxGlobal = maxCurrent;
+    }
+  }
+  return maxGlobal;
+}
+
+// console.log(maxSubArr([1, 1, 2, 2, 3, 4, 5, 5]));
+
+function reversedStr(str) {
+  str = [...str];
+
+  for (let i = 0; i < Math.round(str.length / 2); i++) {
+    [str[i], str[str.length - i - 1]] = [str[str.length - i - 1], str[i]];
+  }
+
+  return str.join("");
+}
+
+// console.log(reversedStr("qwerty"));
+
+function isValidParntheses(str) {
+  if (!str.length) return null;
+
+  const map = {
+    "{": "}",
+    "[": "]",
+    "(": ")",
+  };
+
+  const stack = [];
+
+  for (i of str) {
+    if (Object.keys(map).includes(i)) {
+      stack.push(i);
+    } else {
+      let popped = stack.pop();
+      if (map[popped] !== i) {
+        return false;
+      }
+    }
+  }
+
+  return stack.length === 0;
+}
+
+console.log(isValidParntheses("()")); // Output: true
+console.log(isValidParntheses("{[]}")); // Output: true
+console.log(isValidParntheses("(]")); // Output: false
+console.log(isValidParntheses("([)]")); // Output: false
+console.log(isValidParntheses("([)]{}}{")); // Output: false
