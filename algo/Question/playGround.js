03f093062e1cb182a4f66308bac884cd2ac34a56
unknown <sakarim9124@gmail.com>
2023-10-26 15:04:31 +0530

    updated

--- algo/Question/playGround.js
+++ algo/Question/playGround.js
@@ -1,141 +1,73 @@
-function removeDup(arr) {
-  if (!arr.length) return [];
-
-  arr = arr.sort((a, b) => a - b);
-
-  arr = arr.sort((a, b) => {
-    if (a < b) return -1;
-    if (a > b) return 1;
-    return 0;
-  });
-
-  let pos = 0;
-
-  for (let i = 0; i < arr.length; i++) {
-    if (arr[pos] !== arr[i]) {
-      pos++;
-      arr[pos] = arr[i];
-    }
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.next = null;
+  }
+}
+class Stack {
+  constructor(params) {
+    this.first = null;
+    this.last = null;
+    this.size = 0;
   }
 
-  arr.length = pos + 1;
-
-  return arr;
-}
-
-// console.log(removeDup([1, 1, 2, 2, 3, 3]));
-// console.log(removeDup([1, 2, 31, 4, 1, 3]));
-function twoSum(arr, k) {
-  const obj = {};
-
-  for (let i = 0; i < arr.length; i++) {
-    const temp = k - arr[i];
-
-    if (obj[temp] !== undefined) {
-      return [i, obj[temp]];
-    }
-
-    obj[arr[i]] = i;
-  }
-  return null;
-}
-
-// console.log(twoSum([1, 2, 31, 4, 1, 3], 35));
-
-function twoSumSorted(arr, k) {
-  let left = 0;
-  let right = arr.length - 1;
-
-  while (left < right) {
-    let sum = arr[left] + arr[right];
-
-    if (sum === k) {
-      return [left, right];
-    } else if (sum < k) {
-      left++;
-    } else right--;
-  }
-  // return null;
-}
-
-// console.log(twoSumSorted([1, 1, 2, 2, 3, 3], 5));
-function maxSubArr(arr) {
-  let maxCurrent = arr[0];
-  let maxGlobal = arr[0];
-
-  for (let i = 1; i < arr.length; i++) {
-    maxCurrent = Math.max(arr[i], maxCurrent + arr[i]);
-    if (maxCurrent > maxGlobal) {
-      maxGlobal = maxCurrent;
-    }
-  }
-  return maxGlobal;
-}
-
-const exampleArr1 = [-2, 1, -3, 4, -1, 2, 1, -5, 4];
-// console.log(maxSubArr(exampleArr1)); // Result: 6 (subarray [4, -1, 2, 1])
-
-// Example 2
-// const exampleArr2 = [1, 2, 3, 4, 5];
-// const result2 = maxSubArr(exampleArr2); // Result: 15 (entire array is the maximum subarray)
-
-// Example 3
-// const exampleArr3 = [-1, -2, -3, -4, -5];
-// const result3 = maxSubArr(exampleArr3); // Result: -1 (the maximum subarray is a single element)
-function revStr(str) {
-  if (!str.length) return "";
-
-  str = [...str];
-
-  for (let i = 0; i < Math.round(str.length / 2); i++) {
-    [str[i], str[str.length - i - 1]] = [str[str.length - i - 1], str[i]];
-  }
-
-  return str.join("");
-}
-
-// console.log(revStr("hello"));
-
-function validParenthese(str) {
-  const map = {
-    "{": "}",
-    "[": "]",
-    "(": ")",
-  };
-
-  const stack = [];
-
-  for (i of str) {
-    if (Object.keys(map).includes(i)) {
-      stack.push(i);
+  push(value) {
+    const newNode = new Node(value);
+    if (!this.first) {
+      this.first = newNode;
+      this.last = newNode;
     } else {
-      const poppedNode = stack.pop();
-
-      if (!map[poppedNode]) return false;
+      newNode.next = this.first;
+      this.first = newNode;
     }
+    ++this.size;
   }
-  return stack.length == 0;
+  pop() {
+    if (!this.first) return null;
+    else {
+      this.first = this.first.next;
+    }
+    --this.size;
+  }
 }
 
-function validParenthesesOwn(str) {
-  const stack = [];
-  const map = {
-    "{": "}",
-    "[": "]",
-    "(": ")",
-  };
+// const stackOne = new Stack();
+// stackOne.push(11);
+// stackOne.push(22);
+// stackOne.push(33);
+// console.log(stackOne);
+// stackOne.pop();
+// console.log(stackOne);
 
-  for (i of str) {
-    if (Object.keys(map).includes(i)) {
-      stack.push(i);
+class Queue {
+  constructor(params) {
+    this.first = null;
+    this.last = null;
+    this.size = 0;
+  }
+
+  enqueue(value) {
+    const newNode = new Node(value);
+    if (!this.first) {
+      this.first = newNode;
+      this.last = newNode;
     } else {
-      const popped = stack.pop();
-      if (map[popped] !== i) return false;
+      this.last.next = newNode;
+      this.last = newNode;
     }
+    ++this.size;
   }
-
-  return stack.length == 0;
+  deque() {
+    if (!this.first) return null;
+    else {
+      this.first = this.first.next;
+    }
+    --this.size;
+  }
 }
 
-console.log(validParenthese("(()"));
-console.log(validParenthesesOwn("(()"));
+const queueOne = new Queue();
+queueOne.enqueue(11);
+queueOne.enqueue(22);
+queueOne.enqueue(33);
+console.log(queueOne, " qqqq");
