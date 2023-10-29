65732d707de9b31f093e9fe913af9db94ea213f6
unknown <sakarim9124@gmail.com>
2023-10-29 19:01:41 +0530

    updated

--- react_interview/boilerplate/react_playground/src/App.js
+++ react_interview/boilerplate/react_playground/src/App.js
@@ -1,11 +1,19 @@
-import React from "react";
-import Parent from "./test/Parent";
-const App = () => {
+import React, { useState } from "react";
+
+function Counter() {
+  const [count, setCount] = useState(0);
+
+  const increment = () => {
+    // Correct location to call setCount
+    setCount(count + 1);
+  };
+
   return (
     <div>
-      <Parent />
+      <p>Count: {count}</p>
+      <button onClick={increment}>Increment</button>
     </div>
   );
-};
+}
 
-export default App;
+export default Counter;
