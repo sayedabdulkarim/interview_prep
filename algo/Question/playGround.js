05d0c3662460a01c2b35e0f3a58d9a4ac0cf26a7
unknown <sakarim9124@gmail.com>
2023-10-31 00:43:49 +0530

    updated

--- algo/Question/playGround.js
+++ algo/Question/playGround.js
@@ -5,41 +5,66 @@ class Node {
   }
 }
 
-class CircleLinkedList {
-  constructor(params) {
-    this.head = null;
-    this.tail = null;
-    this.length = 0;
+class Stack {
+  constructor() {
+    this.top = null;
+    this.size = 0;
   }
 
   push(value) {
     const newNode = new Node(value);
 
-    if (!this.head) {
-      this.head = newNode;
-      this.tail = newNode;
+    if (!this.top) {
+      this.top = newNode;
     } else {
-      newNode.next = this.head;
-      this.tail.next = newNode;
-      this.tail = newNode;
+      newNode.next = this.top;
+      this.top = newNode;
     }
-    ++this.length;
+    ++this.size;
   }
 
-  traverse(idx) {
-    let currentNode = this.head;
-
-    for (let i = 0; i < idx; i++) {
-      currentNode = currentNode.next;
+  pop() {
+    if (!this.top) {
+      return null; // Stack is empty
     }
-
-    return currentNode;
+    const poppedValue = this.top.value;
+    this.top = this.top.next;
+    --this.size;
+    return poppedValue;
   }
 }
 
-const circleOne = new CircleLinkedList();
-circleOne.push(11);
-circleOne.push(22);
-circleOne.push(33);
+class QueueUsingStacks {
+  constructor() {
+    this.stack1 = new Stack();
+    this.stack2 = new Stack();
+  }
 
-console.log(circleOne);
+  enqueue(value) {
+    this.stack1.push(value);
+  }
+
+  dequeue() {
+    if (this.stack2.size === 0) {
+      if (this.stack1.size === 0) {
+        return "Queue is empty";
+      }
+      while (this.stack1.size > 0) {
+        this.stack2.push(this.stack1.pop());
+      }
+    }
+    return this.stack2.pop();
+  }
+}
+
+const myQueue = new QueueUsingStacks();
+myQueue.enqueue(1);
+myQueue.enqueue(2);
+myQueue.enqueue(3);
+
+// console.log(myQueue.stack1, " myQueue 1");
+console.log(myQueue.dequeue());
+console.log(myQueue.dequeue());
+console.log(myQueue.dequeue());
+
+// console.log(myQueue.stack2, " myQueue 1 after");
