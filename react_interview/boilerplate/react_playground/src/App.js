99091dec458ee8459ed20acd25e8173e148a8ac7
unknown <sakarim9124@gmail.com>
2023-10-31 10:29:26 +0530

    updated

--- react_interview/boilerplate/react_playground/src/App.js
+++ react_interview/boilerplate/react_playground/src/App.js
@@ -1,19 +1,7 @@
-import React, { useState } from "react";
+import React from "react";
 
-function Counter() {
-  const [count, setCount] = useState(0);
+const App = () => {
+  return <div>App</div>;
+};
 
-  const increment = () => {
-    // Correct location to call setCount
-    setCount(count + 1);
-  };
-
-  return (
-    <div>
-      <p>Count: {count}</p>
-      <button onClick={increment}>Increment</button>
-    </div>
-  );
-}
-
-export default Counter;
+export default App;
--- react_interview/boilerplate/react_playground/src/test/ChildTwo.js
+++
@@ -1,9 +0,0 @@
-import React, { memo } from "react";
-
-const ChildTwo = () => {
-  console.log("calling from child Two");
-
-  return <div>ChildTwo</div>;
-};
-
-export default memo(ChildTwo);
