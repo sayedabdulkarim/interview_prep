ecf0ede44c4626dd198032e9f41ad0690d7e6a26
unknown <sakarim9124@gmail.com>
2023-11-15 16:32:01 +0530

    updated

--- algo/Question/playGround.js
+++ algo/Question/playGround.js
@@ -83,8 +83,71 @@ function moveZeroesToRight(arr) {
 
 // console.log(moveZeroesToLeft([3, 0, 2, 11, 0, 2, 4, 0, 33]));
 
-function sort(arr) {
+function sortAsc(arr) {
   let isSorted = false;
+
+  while (!isSorted) {
+    isSorted = true;
+
+    for (let i = 0; i < arr.length; i++) {
+      if (arr[i] > arr[i + 1]) {
+        const temp = arr[i];
+        arr[i] = arr[i + 1];
+        arr[i + 1] = temp;
+        isSorted = false;
+      }
+    }
+  }
+  return arr;
 }
 
-// console.log(moveZeroesToLeft([3, 0, 2, 11, 0, 2, 4, 0, 33]));
+// console.log(sortAsc([3, 0, 2, 11, 0, 2, 4, 0, 33]));
+
+function removeDup(arr) {
+  if (!arr.length) return [];
+
+  arr.sort((a, b) => a - b);
+
+  let count = 0;
+
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[count] !== arr[i]) {
+      count++;
+      arr[count] = arr[i];
+    }
+  }
+
+  arr.length = count + 1;
+
+  return { arr, count };
+}
+
+// console.log(removeDup([3, 0, 2, 11, 0, 2, 4, 0, 33]));
+
+function chunkArray(arr, num) {
+  const result = [];
+  for (let i = 0; i < arr.length; i += num) {
+    console.log(i);
+    const sliced = arr.slice(i, num + i);
+    result.push(sliced);
+  }
+  return result;
+}
+
+// console.log(chunkArray([3, 0, 2, 11, 0, 2, 2, 2, 3, 4, 0, 33], 2));
+
+function twoSum(arr, k) {
+  const obj = {};
+
+  for (let i = 0; i < arr.length; i++) {
+    const temp = k - arr[i];
+    if (obj[temp] !== undefined) {
+      return [obj[temp], i];
+    }
+
+    obj[arr[i]] = i;
+  }
+  return null;
+}
+
+console.log(twoSum([3, 0, 2, 11, 0, 2, 2, 2, 3, 4, 0, 33], 14));
