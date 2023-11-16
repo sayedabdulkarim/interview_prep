c46212b1664bad94e659f5454d0bca5319610d8f
unknown <sakarim9124@gmail.com>
2023-11-16 11:19:58 +0530

    updated

--- algo/Question/playGround.js
+++ algo/Question/playGround.js
@@ -1,108 +1,3 @@
-function maxElem(arr) {
-  for (let i = 0; i < arr.length; i++) {
-    if (arr[0] > arr[i]) {
-      [arr[0], arr[i]] = [arr[i], arr[0]];
-    }
-  }
-
-  return arr;
-}
-
-// console.log(maxElem([3, 2, 11, 2, 4, 0, 33]));
-function reverseArr(arr) {
-  if (!arr.length) return [];
-
-  for (let i = 0; i < Math.round(arr.length / 2); i++) {
-    [arr[i], arr[arr.length - i - 1]] = [arr[arr.length - i - 1], arr[i]];
-  }
-
-  return arr;
-}
-
-// console.log(reverseArr([3, 2, 11, 2, 4, 0, 33]));
-function moveZeroesToRight(arr) {
-  if (!arr.length) return [];
-
-  let count = 0;
-
-  for (let i = 0; i < arr.length; i++) {
-    if (arr[i] !== 0) {
-      arr[count] = arr[i];
-      count++;
-    }
-  }
-
-  for (let i = count; i < arr.length; i++) {
-    arr[i] = 0;
-  }
-
-  return arr;
-}
-
-// console.log(moveZeroesToRight([3, 0, 2, 11, 2, 4, 0, 33]));
-
-// function moveZeroesToLeft(arr) {
-//   if (!arr.length) return [];
-
-//   let count = arr.length - 1;
-
-//   for (let i = arr.length - 1; i >= 0; i--) {
-//     if (arr[i] !== 0) {
-//       arr[count] = arr[i];
-//       count--;
-//     }
-//   }
-
-//   for (let i = count; i >= 0; i--) {
-//     arr[i] = 0;
-//   }
-
-//   return {
-//     count,
-//     arr,
-//   };
-// }
-
-// function moveZeroesToLeft(arr) {
-//   if (!arr) return [];
-
-//   let count = arr.length - 1;
-
-//   for (let i = arr.length - 1; i >= 0; i--) {
-//     if (arr[i] !== 0) {
-//       arr[count] = arr[i];
-//       count--;
-//     }
-//   }
-//   //   console.log(arr, " arrr");
-//   //   for (let i = count; i >= 0; i--) {
-//   //     arr[i] = 0;
-//   //   }
-//   return arr;
-// }
-
-// console.log(moveZeroesToLeft([3, 0, 2, 11, 0, 2, 4, 0, 33]));
-
-function sortAsc(arr) {
-  let isSorted = false;
-
-  while (!isSorted) {
-    isSorted = true;
-
-    for (let i = 0; i < arr.length; i++) {
-      if (arr[i] > arr[i + 1]) {
-        const temp = arr[i];
-        arr[i] = arr[i + 1];
-        arr[i + 1] = temp;
-        isSorted = false;
-      }
-    }
-  }
-  return arr;
-}
-
-// console.log(sortAsc([3, 0, 2, 11, 0, 2, 4, 0, 33]));
-
 function removeDup(arr) {
   if (!arr.length) return [];
 
@@ -119,22 +14,10 @@ function removeDup(arr) {
 
   arr.length = count + 1;
 
-  return { arr, count };
+  return arr;
 }
 
-// console.log(removeDup([3, 0, 2, 11, 0, 2, 4, 0, 33]));
-
-function chunkArray(arr, num) {
-  const result = [];
-  for (let i = 0; i < arr.length; i += num) {
-    console.log(i);
-    const sliced = arr.slice(i, num + i);
-    result.push(sliced);
-  }
-  return result;
-}
-
-// console.log(chunkArray([3, 0, 2, 11, 0, 2, 2, 2, 3, 4, 0, 33], 2));
+// console.log(removeDup([2, 1, 2, 3, 4, 6, 2, 0, 0]));
 
 function twoSum(arr, k) {
   const obj = {};
@@ -147,64 +30,50 @@ function twoSum(arr, k) {
 
     obj[arr[i]] = i;
   }
+
   return null;
 }
 
-// console.log(twoSum([3, 0, 2, 11, 0, 2, 2, 2, 3, 4, 0, 33], 14));
+// console.log(twoSum([2, 1, 2, 3, 4, 6, 2, 0, 0], 4));
 
-const arr = [
-  { phase: 1, name: "phase One", age: 1 },
-  { phase: 2, name: "phase Two", age: 2 },
-  { phase: 1, name: "phase One", age: 1 },
-  { phase: 2, name: "phase Two", age: 2 },
-  { phase: 1, name: "phase One", age: 1 },
-];
+function twoSumSorted(arr, k) {
+  if (!arr.length) return null;
 
-json = {
-  phase1: [
-    { phase: 1, name: "phase One", age: 1 },
-    { phase: 1, name: "phase One", age: 1 },
-    { phase: 1, name: "phase One", age: 1 },
-  ],
-  phase2: [
-    { phase: 2, name: "phase Two", age: 2 },
-    { phase: 2, name: "phase Two", age: 2 },
-  ],
-};
+  let left = 0;
+  let right = arr.length - 1;
 
-// function getJson(arr) {
-//     const data = {};
+  while (left < right) {
+    let sum = arr[left] + arr[right];
 
-//     arr.forEach(item => {
-//       // Construct the key name, e.g., 'phase1', 'phase2', etc.
-//       const key = `phase${item.phase}`;
-
-//       // Initialize the array if this key hasn't been seen before
-//       if (!data[key]) {
-//         data[key] = [];
-//       }
-
-//       // Push the current item onto the array for this key
-//       data[key].push(item);
-//     });
-
-//     return data;
-//   }
-
-function getJson(arr) {
-  const data = {};
-
-  for (let i = 0; i < arr.length; i++) {
-    const key = `phase${arr[i].phase}`;
-
-    if (!data[key]) {
-      data[key] = [];
-    }
-
-    data[key].push(arr[i]);
+    if (sum === k) {
+      return [left, right];
+    } else if (sum < k) {
+      left++;
+    } else right--;
   }
-
-  return data;
+  return null;
 }
 
-console.log(getJson(arr));
+// console.log(twoSumSorted([1, 4, 5, 8, 8, 9, 13], 22));
+
+function maxSubArr(arr) {
+  if (!arr.length) return [];
+  let maxCurrent = arr[0];
+  let maxGlobal = arr[0];
+
+  for (let i = 1; i < arr.length; i++) {
+    maxCurrent = Math.max(arr[i], maxCurrent + arr[i]);
+    if (maxCurrent > maxGlobal) {
+      maxGlobal = maxCurrent;
+    }
+  }
+  return maxGlobal;
+}
+
+// console.log(maxSubArr([-2, 1, -3, 4, -1, 2, 1, -5, 4]));
+
+const currying = (a) => (b) => b ? currying(a + b) : a;
+
+// console.log(currying(5)(10)(20)());
+
+// console.log(maxSubArr([-2, 1, -3, 4, -1, 2, 1, -5, 4]));
