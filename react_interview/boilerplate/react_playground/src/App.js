50c8ed886f088a1913970f02a87119f1e8a5b515
unknown <sakarim9124@gmail.com>
2024-01-14 08:46:23 +0530

    updated

--- react_interview/boilerplate/react_playground/src/App.js
+++ react_interview/boilerplate/react_playground/src/App.js
@@ -1,32 +1,79 @@
-import React from "react";
-
-import Parent from "./components/renderProps/Parent";
-import ChildOne from "./components/renderProps/ChildOne";
-import ChildTwo from "./components/renderProps/ChildTwo";
+import React, { useEffect, useState } from "react";
 
 const App = () => {
+  //state
+  const [seconds, setSeconds] = useState(0);
+  const [minutes, setMinutes] = useState(0);
+  const [hours, setHours] = useState(0);
+  const [isStart, setIsStart] = useState(false);
+  //func
+  const handleStart = (val) => {
+    setIsStart(val);
+  };
+
+  const timer = (sec, min, hour) => {
+    if (sec > 0) {
+      setMinutes((prev) => prev - 1);
+    } else if (sec === 0 && min >= 1) {
+      setSeconds(59);
+      setMinutes((prev) => prev - 1);
+      setHours((prev) => prev - 1);
+    } else {
+      setHours((prev) => prev - 1);
+      setMinutes(59);
+      setSeconds(59);
+    }
+  };
+
+  //async
+  useEffect(() => {
+    if (isStart) {
+      let isTimer = setInterval(() => {
+        timer(seconds, minutes, hours);
+      }, 1000);
+      return () => clearInterval(isTimer);
+    }
+  }, [isStart, seconds, minutes, hours]);
+
+  console.log({
+    hours: typeof hours,
+    minutes: typeof minutes,
+    seconds: typeof seconds,
+  });
   return (
     <div>
-      <h1>Hello</h1>
-      <h1>Hello</h1>
-
-      <Parent
-        render={(count, handleCount) => (
-          <ChildOne count={count} handleCount={handleCount} />
-        )}
-      />
-      <hr />
-      <hr />
-      <hr />
-      <hr />
-      <hr />
-      <Parent
-        render={(count, handleCount) => (
-          <ChildTwo count={count} handleCount={handleCount} />
-        )}
-      />
-
-      {/* <ChildOne /> */}
+      {!isStart ? (
+        <div>
+          <input
+            type="number"
+            placeholder="Enter Hour"
+            onChange={(e) => setHours(+e.target.value)}
+          />
+          <hr />
+          <input
+            type="number"
+            placeholder="Enter Minutes"
+            onChange={(e) => setMinutes(+e.target.value)}
+          />
+          <hr />
+          <input
+            type="number"
+            placeholder="Enter Seconds"
+            onChange={(e) => setSeconds(+e.target.value)}
+          />
+          <hr />
+          <hr />
+          <button onClick={() => handleStart(true)}>Start</button>
+        </div>
+      ) : (
+        <>
+          <h2>Hour: {hours}</h2>
+          <h2>Minutes: {minutes}</h2>
+          <h2>Seconds: {seconds}</h2>
+          <button onClick={() => console.log("pause")}>Pause</button>
+          <button onClick={() => handleStart(false)}>Stop</button>
+        </>
+      )}
     </div>
   );
 };
