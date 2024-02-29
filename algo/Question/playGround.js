52937b98ad6b54d2fde812690262250b612eec8f
unknown <sakarim9124@gmail.com>
2024-02-29 18:33:27 +0530

    updated

--- algo/Question/playGround.js
+++ algo/Question/playGround.js
@@ -2,6 +2,76 @@
 // const array = [10, 9, 8, 7, 6];
 const array = [64, 34, 25, 12, 22, 0, 0, 11, 90, 100, -100, 64];
 
+function maxElem(arr) {
+  if (!arr.length) return null;
+
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[0] < arr[i]) {
+      [arr[0], arr[i]] = [arr[i], arr[0]];
+    }
+  }
+  return arr[0];
+}
+
+function minElem(arr) {
+  if (!arr.length) return null;
+
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[0] > arr[i]) {
+      [arr[0], arr[i]] = [arr[i], arr[0]];
+    }
+  }
+  return arr[0];
+}
+
+function reverseArr(arr) {
+  if (!arr.length) return null;
+
+  for (let i = 0; i < Math.round(arr.length / 2); i++) {
+    // [ arr[i], [arr.length - i-1]] =  [ [arr.length - i-1], arr[i]]
+    [arr[i], arr[arr.length - i - 1]] = [arr[arr.length - i - 1], arr[i]];
+  }
+  return arr;
+}
+
+function moveZeroesToRight(arr) {
+  if (!arr.length) return null;
+
+  let count = 0;
+
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] !== 0) {
+      arr[count] = arr[i];
+      count++;
+    }
+  }
+
+  for (let i = count; i < arr.length; i++) {
+    arr[i] = 0;
+  }
+
+  return arr;
+}
+
+function moveZeroesToLeft(arr) {
+  if (!arr.length) return null;
+
+  let count = 0;
+
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] !== 0) {
+      arr[count] = arr[i];
+      count++;
+    }
+  }
+
+  for (let i = 0; i < arr.length - count; i++) {
+    arr[i] = 0;
+  }
+
+  return arr;
+}
+
 function sortAsc(arr) {
   let isSorted = false;
 
@@ -13,7 +83,6 @@ function sortAsc(arr) {
         const temp = arr[i];
         arr[i] = arr[i + 1];
         arr[i + 1] = temp;
-
         isSorted = false;
       }
     }
@@ -45,27 +114,30 @@ function removeDup(arr) {
   const newSet = new Set(arr);
   const newArr = Array.from(newSet);
 
-  return newArr.length === arr.length;
+  return newArr;
 }
 
 function chunkArray(arr, num) {
-  const chunk = [];
+  if (!arr.length) return null;
+
+  const chunkArr = [];
 
   for (let i = 0; i < arr.length; i += num) {
-    console.log(i);
     const sliced = arr.slice(i, i + num);
-    chunk.push(sliced);
+    chunkArr.push(sliced);
   }
-  return chunk;
+
+  return chunkArr;
 }
 
-// chunkArray(array, 3);
-
 console.log({
+  // maxElem: maxElem(array),
+  // minElem: minElem(array),
+  // reverseArr: reverseArr(array),
+  // moveZeroesToRight: moveZeroesToRight(array),
+  // moveZeroesToLeft: moveZeroesToLeft(array),
   // sortAsc: sortAsc(array),
   // sortDesc: sortDesc(array),
   // removeDup: removeDup(array),
-  chunkArray: chunkArray(array, 5),
+  chunkArray: chunkArray(array, 8),
 });
-
-// console.log(array, " aaa");
