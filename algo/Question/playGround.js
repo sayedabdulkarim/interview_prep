b9e57fb65d3e03bde69abff73aa12d18ebf0f7b2
unknown <sakarim9124@gmail.com>
2024-03-05 18:18:04 +0530

    updated

--- algo/Question/playGround.js
+++ algo/Question/playGround.js
@@ -10,7 +10,8 @@ function maxElem(arr) {
       [arr[0], arr[i]] = [arr[i], arr[0]];
     }
   }
-  return arr[0];
+
+  return arr;
 }
 
 function minElem(arr) {
@@ -21,123 +22,23 @@ function minElem(arr) {
       [arr[0], arr[i]] = [arr[i], arr[0]];
     }
   }
-  return arr[0];
+
+  return arr;
 }
 
 function reverseArr(arr) {
   if (!arr.length) return null;
 
   for (let i = 0; i < Math.round(arr.length / 2); i++) {
-    // [ arr[i], [arr.length - i-1]] =  [ [arr.length - i-1], arr[i]]
-    [arr[i], arr[arr.length - i - 1]] = [arr[arr.length - i - 1], arr[i]];
-  }
-  return arr;
-}
-
-function moveZeroesToRight(arr) {
-  if (!arr.length) return null;
-
-  let count = 0;
-
-  for (let i = 0; i < arr.length; i++) {
-    if (arr[i] !== 0) {
-      arr[count] = arr[i];
-      count++;
-    }
-  }
-
-  for (let i = count; i < arr.length; i++) {
-    arr[i] = 0;
-  }
-
-  return arr;
-}
-
-function moveZeroesToLeft(arr) {
-  if (!arr.length) return null;
-
-  let count = 0;
-
-  for (let i = 0; i < arr.length; i++) {
-    if (arr[i] !== 0) {
-      arr[count] = arr[i];
-      count++;
-    }
-  }
-
-  for (let i = 0; i < arr.length - count; i++) {
-    arr[i] = 0;
-  }
-
-  return arr;
-}
-
-function sortAsc(arr) {
-  let isSorted = false;
-
-  while (!isSorted) {
-    isSorted = true;
-
-    for (let i = 0; i < arr.length; i++) {
-      if (arr[i] > arr[i + 1]) {
-        const temp = arr[i];
-        arr[i] = arr[i + 1];
-        arr[i + 1] = temp;
-        isSorted = false;
-      }
+    if (arr[i] > arr[i + 1]) {
+      [arr[i], arr[arr.length - i - 1]] = [arr[arr.length - i - 1], arr[i]];
     }
   }
   return arr;
-}
-
-function sortDesc(arr) {
-  let isSorted = false;
-
-  while (!isSorted) {
-    isSorted = true;
-
-    for (let i = 0; i < arr.length; i++) {
-      if (arr[i] < arr[i + 1]) {
-        const temp = arr[i];
-        arr[i] = arr[i + 1];
-        arr[i + 1] = temp;
-        isSorted = false;
-      }
-    }
-  }
-  return arr;
-}
-
-function removeDup(arr) {
-  if (!arr.length) return null;
-
-  const newSet = new Set(arr);
-  const newArr = Array.from(newSet);
-
-  return newArr;
-}
-
-function chunkArray(arr, num) {
-  if (!arr.length) return null;
-
-  const chunkArr = [];
-
-  for (let i = 0; i < arr.length; i += num) {
-    const sliced = arr.slice(i, i + num);
-    chunkArr.push(sliced);
-  }
-
-  return chunkArr;
 }
 
 console.log({
-  // maxElem: maxElem(array),
   // minElem: minElem(array),
-  // reverseArr: reverseArr(array),
-  // moveZeroesToRight: moveZeroesToRight(array),
-  // moveZeroesToLeft: moveZeroesToLeft(array),
-  // sortAsc: sortAsc(array),
-  // sortDesc: sortDesc(array),
-  // removeDup: removeDup(array),
-  chunkArray: chunkArray(array, 8),
+  // maxElem: maxElem(array),
+  reverseArr: reverseArr(array),
 });
