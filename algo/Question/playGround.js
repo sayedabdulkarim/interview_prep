883bdaa2522833588b102a10339ed6b871313f82
unknown <sakarim9124@gmail.com>
2024-03-11 22:14:41 +0530

    updated

--- algo/Question/playGround.js
+++ algo/Question/playGround.js
@@ -3,170 +3,55 @@
 const array = [64, 34, 25, 12, 22, 0, 0, 11, 90, 100, -100, 64];
 const string = "A man a plan a canal Panama";
 
-function maxElem(arr) {
-  if (!arr.length) return null;
-
-  for (let i = 0; i < arr.length; i++) {
-    if (arr[0] < arr[i]) {
-      [arr[0], arr[i]] = [arr[i], arr[0]];
-    }
-  }
-
-  return arr;
-}
-
-function minElem(arr) {
-  if (!arr.length) return null;
-
-  for (let i = 0; i < arr.length; i++) {
-    if (arr[0] > arr[i]) {
-      [arr[0], arr[i]] = [arr[i], arr[0]];
-    }
-  }
-
-  return arr;
-}
-
-function reverseArr(arr) {
-  if (!arr.length) return null;
-
-  for (let i = 0; i < Math.round(arr.length / 2); i++) {
-    [arr[i], arr[arr.length - i - 1]] = [arr[arr.length - i - 1], arr[i]];
-  }
-
-  return arr;
-}
-
-function moveZeroesToRight(arr) {
-  if (!arr.length) return null;
-
-  let count = 0;
-  for (let i = 0; i < arr.length; i++) {
-    if (arr[i] !== 0) {
-      arr[count] = arr[i];
-      count++;
-    }
-  }
-
-  for (let i = count; i < arr.length; i++) {
-    arr[i] = 0;
-  }
-
-  return {
-    count,
-    arr,
-  };
-}
-
-function moveZeroesToLeft(arr) {
-  if (!arr.length) return null;
-
-  let count = 0;
-  for (let i = 0; i < arr.length; i++) {
-    if (arr[i] !== 0) {
-      arr[count] = arr[i];
-      count++;
-    }
-  }
-
-  for (let i = 0; i < arr.length - count; i++) {
-    arr[i] = 0;
-  }
-
-  return {
-    count,
-    arr,
-  };
-}
-
-function sortAsc(arr) {
-  let isSorted = false;
-
-  while (!isSorted) {
-    isSorted = true;
-    for (let i = 0; i < arr.length; i++) {
-      if (arr[i] > arr[i + 1]) {
-        const temp = arr[i];
-        arr[i] = arr[i + 1];
-        arr[i + 1] = temp;
-
-        isSorted = false;
-      }
-    }
-  }
-
-  return arr;
-}
-
-function sortDesc(arr) {
-  let isSorted = false;
-
-  while (!isSorted) {
-    isSorted = true;
-    for (let i = 0; i < arr.length; i++) {
-      if (arr[i] < arr[i + 1]) {
-        const temp = arr[i];
-        arr[i] = arr[i + 1];
-        arr[i + 1] = temp;
-
-        isSorted = false;
-      }
-    }
-  }
-
-  return arr;
-}
-
-function removeDup(arr) {
-  let newSet = new Set(arr);
-  let newArr = Array.from(newSet);
-  return newArr;
-}
-
-function containsDuplicate(arr) {
-  function getObj(array) {
-    const obj = {};
-
-    for (let i of arr) {
-      obj[i] = (obj[i] || 0) + 1;
-    }
-
-    return obj;
-  }
-
-  return getObj(arr);
-}
-
-function chunkArray(arr, num) {
-  const chunkArray = [];
-  for (let i = 0; i < arr.length; i += num) {
-    // console.log(i, " ii");
-    const sliced = arr.slice(i, i + num);
-    chunkArray.push(sliced);
-  }
-  return chunkArray;
-}
-
 function revStr(str) {
+  if (!str.length) return null;
+
   str = [...str];
 
   for (let i = 0; i < Math.round(str.length / 2); i++) {
     [str[i], str[str.length - i - 1]] = [str[str.length - i - 1], str[i]];
   }
-
   return str.join("");
 }
 
+// function cleanString(str) {
+//   let cleaned = "";
+
+//   for (let i = 0; i < str.length; i++) {
+//     const temp = str[i].toLowerCase();
+//     if ((temp >= "a" || temp <= "z") && (temp >= "0" || temp <= "9")) {
+//       cleaned += temp;
+//     }
+//   }
+//   return cleaned;
+// }
+
+// function isPalindrome(str) {
+//   // const cleaned = cleanedStr(str);
+//   const cleaned = cleanString(str);
+//   return str === [...cleaned].reverse().join("");
+// }
+
+function cleanString(str) {
+  let cleanedStr = "";
+  for (let i = 0; i < str.length; i++) {
+    const char = str[i].toLowerCase();
+    if ((char >= "a" && char <= "z") || (char >= "0" && char <= "9")) {
+      cleanedStr += char;
+    }
+  }
+  return cleanedStr;
+}
+
+function isPalindrome(str) {
+  const cleanedStr = cleanString(str);
+  console.log(cleanedStr, " cleaned");
+  const reversedStr = cleanedStr.split("").reverse().join("");
+  return cleanedStr === reversedStr;
+}
+
 console.log({
-  // maxElem: maxElem(array),
-  // minElem: minElem(array),
-  // reverseArr: reverseArr(array),
-  // moveZeroesToRight: moveZeroesToRight(array),
-  // moveZeroesToLeft: moveZeroesToLeft(array),
-  // sortAsc: sortAsc(array),
-  // sortDesc: sortDesc(array),
-  // removeDup: removeDup(array),
-  // containsDuplicate: containsDuplicate(array),
-  // chunkArray: chunkArray(array, 4),
-  revStr: revStr("hello"),
+  // revStr: revStr("string"),
+  // cleanedStr: cleanedStr(string),
+  isPalindrome: isPalindrome(string),
 });
