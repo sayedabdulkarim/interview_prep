3ee7e2c0e6def59b2f069a74c15939b5e7502f3b
unknown <sakarim9124@gmail.com>
2024-03-20 16:36:01 +0530

    updaed

--- algo/Question/playGround.js
+++ algo/Question/playGround.js
@@ -2,9 +2,3 @@
 // const array = [10, 9, 8, 7, 6];
 const array = [64, 34, 25, 12, 22, 0, 0, 11, 90, 100, -100, 64];
 const string = "A man a plan a canal Panama";
-
-function reversedStr(str) {
-  if (!str.length) return null;
-
-  str = [...str];
-}
