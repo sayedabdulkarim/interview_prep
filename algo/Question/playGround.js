33b00eea8fdb78840d3093b3bccac2d278f5198d
unknown <sakarim9124@gmail.com>
2024-03-22 09:20:48 +0530

    updated

--- algo/Question/playGround.js
+++ algo/Question/playGround.js
@@ -27,3 +27,28 @@ function maxElem(arr) {
   return arr[0];
   // return arr[arr.length - 1];
 }
+
+function maxElem(nums) {
+  if (nums.length === 0) return null; // Handle empty array
+  let max = nums[0]; // Initialize with the first element
+
+  for (let i = 1; i < nums.length; i++) {
+    // Start loop from 1 as nums[0] is already in max
+    if (nums[i] > max) max = nums[i];
+  }
+  return max;
+}
+
+function maxElem(arr) {
+  if (!arr.length) return null;
+
+  for (let i = 1; i < arr.length; i++) {
+    if (arr[i] > arr[0]) {
+      // Swap the elements
+      [arr[0], arr[i]] = [arr[i], arr[0]];
+      // [arr[i], arr[i + 1]] = [arr[i + 1], arr[i]];
+    }
+  }
+  return arr[0];
+  // return arr[arr.length - 1];
+}
