46a45458b8a3b35642bc60a6b4a94cc2c62a525a
unknown <sakarim9124@gmail.com>
2024-04-18 23:52:19 +0530

    updated

--- algo/Question/playGround.js
+++ algo/Question/playGround.js
@@ -3,27 +3,20 @@
 const array = [64, 34, 25, 12, 22, 0, 0, 11, 90, 100, -100, 64];
 const string = "A man a plan a canal Panama";
 
-function maxElem(nums) {
-  if (nums.length === 0) return null; // Handle empty array
-  let max = nums[0]; // Initialize with the first element
-
-  for (let i = 1; i < nums.length; i++) {
-    // Start loop from 1 as nums[0] is already in max
-    if (nums[i] > max) max = nums[i];
+function numberToExcelColumn(number) {
+  let result = "";
+  while (number > 0) {
+    const remainder = (number - 1) % 26;
+    result = String.fromCharCode(65 + remainder) + result;
+    number = Math.floor((number - 1) / 26);
   }
-  return max;
+  return result;
 }
 
-function maxElem(arr) {
-  if (!arr.length) return null;
-
-  for (let i = 1; i < arr.length; i++) {
-    if (arr[i] > arr[0]) {
-      // Swap the elements
-      [arr[0], arr[i]] = [arr[i], arr[0]];
-      // [arr[i], arr[i + 1]] = [arr[i + 1], arr[i]];
-    }
+function convertToExcelLabels(numbers) {
+  const excelLabels = [];
+  for (let i = 0; i < numbers.length; i++) {
+    excelLabels.push(numberToExcelColumn(numbers[i]));
   }
-  return arr[0];
-  // return arr[arr.length - 1];
+  return excelLabels;
 }
