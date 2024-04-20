f0e270d6ed7aab1c92b22cd64fa3456e01b19fea
unknown <sakarim9124@gmail.com>
2024-04-20 23:44:17 +0530

    updated

--- algo/Question/playGround.js
+++ algo/Question/playGround.js
@@ -2,3 +2,21 @@
 // const array = [10, 9, 8, 7, 6];
 const array = [64, 34, 25, 12, 22, 0, 0, 11, 90, 100, -100, 64];
 const string = "A man a plan a canal Panama";
+
+function numberToExcelColumn(number) {
+  let result = "";
+  while (number > 0) {
+    const remainder = (number - 1) % 26;
+    result = String.fromCharCode(65 + remainder) + result;
+    number = Math.floor((number - 1) / 26);
+  }
+  return result;
+}
+
+function convertToExcelLabels(numbers) {
+  const excelLabels = [];
+  for (let i = 0; i < numbers.length; i++) {
+    excelLabels.push(numberToExcelColumn(numbers[i]));
+  }
+  return excelLabels;
+}
