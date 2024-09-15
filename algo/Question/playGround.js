3096d939252e2a566aa712fb808af9d25df0a538
unknown <sakarim9124@gmail.com>
2024-09-16 00:56:39 +0530

    updated

--- algo/Question/playGround.js
+++ algo/Question/playGround.js
@@ -2,3 +2,34 @@
 // const array = [10, 9, 8, 7, 6];
 const array = [64, 34, 25, 12, 22, 0, 0, 11, 90, 100, -100, 64];
 const string = "A man a plan a canal Panama";
+
+function KthLargest(nums, n) {
+  //sort
+  function dSort(arr) {
+    let isSorted = false;
+
+    while (!isSorted) {
+      isSorted = true;
+      for (i = 0; i < arr.length; i++) {
+        if (arr[i] < arr[i + 1]) {
+          const temp = arr[i];
+
+          arr[i] = arr[i + 1];
+          arr[i + 1] = temp;
+
+          isSorted = false;
+        }
+      }
+    }
+    return arr;
+  }
+
+  //uniObj
+  const getSortedArr = dSort(nums);
+  const uniObj = new Set(getSortedArr);
+
+  //change to arr
+  const getArr = Array.from(uniObj);
+
+  return getArr[n - 1];
+}
