30f7bff050792a8f886098c101beac8a6e030fab
Sayed Abdul <sayed_abdul_karim@optum.com>
2024-10-20 16:52:53 +0800

    updated

--- react_interview/boilerplate/react_18th_v/src/App.js
+++ react_interview/boilerplate/react_18th_v/src/App.js
@@ -1,60 +1,47 @@
-import React, { useState, useEffect } from "react";
+import React, { forwardRef, useImperativeHandle, useRef } from "react";
 
-const ListItem = ({ item, onRemove }) => {
-  useEffect(() => {
-    console.log(`Item ${item.id} mounted`);
-    return () => console.log(`Item ${item.id} unmounted`);
-  }, []);
+const Parent = () => {
+  const childRef = useRef();
 
+  const handleSubmit = () => {
+    console.log("clicked");
+    childRef.current.testClick();
+    console.log(childRef);
+  };
   return (
     <div>
-      {item.text}
-      <button onClick={() => onRemove(item.id)}>Remove</button>
+      <h1>Parent</h1>
+      <hr />
+      <Children ref={childRef} />
+      <button onClick={handleSubmit}>Click</button>
     </div>
   );
 };
 
-const List = () => {
-  const [items, setItems] = useState([
-    { id: 1, text: "Item 1" },
-    { id: 2, text: "Item 2" },
-    { id: 3, text: "Item 3" },
-  ]);
-
-  const handleAddToEnd = () => {
-    const newItem = {
-      id: Math.random(), // Unique ID
-      text: `Item ${items.length + 1}`,
-    };
-    setItems([...items, newItem]);
-  };
-
-  const handleAddToFront = () => {
-    const newItem = {
-      id: Math.random(), // Unique ID
-      text: `Item 0`,
-    };
-    setItems([newItem, ...items]);
-  };
-
-  const handleRemove = (id) => {
-    setItems(items.filter((item) => item.id !== id));
-  };
-
+const Children = forwardRef((props, ref) => {
+  useImperativeHandle(ref, () => ({
+    testClick() {
+      console.log("clickedd from child");
+    },
+  }));
   return (
     <div>
-      <ul>
-        {items.map((item) => (
-          // First try with key={item.id} then try with key={index} to see the difference
-          <li>
-            <ListItem item={item} onRemove={handleRemove} />
-          </li>
-        ))}
-      </ul>
-      <button onClick={handleAddToEnd}>Add to End</button>
-      <button onClick={handleAddToFront}>Add to Front</button>
+      <h1>Childreenn</h1>
+      <h1>Childreenn</h1>
+      <h1>Childreenn</h1>
+      <h1>Childreenn</h1>
     </div>
   );
-};
+});
+// const Children = () => {
+//   return (
+//     <div>
+//       <h1>Childreenn</h1>
+//       <h1>Childreenn</h1>
+//       <h1>Childreenn</h1>
+//       <h1>Childreenn</h1>
+//     </div>
+//   );
+// };
 
-export default List;
+export default Parent;
