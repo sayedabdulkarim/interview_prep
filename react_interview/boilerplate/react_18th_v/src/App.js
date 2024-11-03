017040440e0831f735efafd4ea21b2219466ca47
Sayed Abdul <sayed_abdul_karim@optum.com>
2024-11-03 16:56:36 +0530

    uopdated

--- react_interview/boilerplate/react_18th_v/src/App.js
+++ react_interview/boilerplate/react_18th_v/src/App.js
@@ -1,40 +1,72 @@
-import React, { forwardRef, useImperativeHandle, useRef } from "react";
+import React, {
+  useDeferredValue,
+  useEffect,
+  useState,
+  useTransition,
+} from "react";
 
-const Parent = () => {
-  //ref
-  const childRef = useRef();
+const genList = () => {
+  const arr = [];
+
+  for (let i = 0; i <= 20000; i++) {
+    arr.push(`Item : ${i}`);
+  }
+
+  return arr;
+};
+
+const allProd = genList();
+
+function filteredProducts(val) {
+  if (!val) return allProd;
+  else {
+    return allProd?.filter((o) => o.includes(val));
+  }
+}
+
+const App = () => {
+  //state
+  const [inpVal, setInpVal] = useState("");
+  const list = filteredProducts(inpVal);
+  const [isPending, startTransition] = useTransition();
 
   //method
-  const handleClick = () => {
-    console.log(childRef.current, " redd");
-    if (childRef?.current?.childClick) {
-      childRef?.current?.childClick();
-    }
+  const handleChange = (e) => {
+    // setInpVal(e.target.value);
+
+    startTransition(() => {
+      setInpVal(e.target.value);
+    });
   };
 
+  //
+  useEffect(() => {}, []);
   return (
     <div>
-      <Children ref={childRef} />
-      <button onClick={handleClick}>Clikc for Child</button>
+      <input
+        type="text"
+        value={inpVal}
+        onChange={handleChange}
+        placeholder="Add Text"
+      />
+      <br />
+      {isPending && <h1>Loading....</h1>}
+      <br />
+      <ListComponent items={list} />
     </div>
   );
 };
 
-const Children = forwardRef((_, ref) => {
-  // useImperativeHandle(ref, () => ({
-  //   childClick() {
-  //     console.log("child clikced");
-  //   },
-  // }));
+const ListComponent = ({ items }) => {
+  const deferredProducts = useDeferredValue(items);
+  return (
+    <ul>
+      {/* {items?.map((o) => { */}
+      {deferredProducts?.map((o) => {
+        return <li key={o}>{o}</li>;
+      })}
+    </ul>
+  );
+};
 
-  useImperativeHandle(ref, () => {
-    return {
-      childClick() {
-        console.log("gotchaaaaa");
-      },
-    };
-  });
-  return <div>Children</div>;
-});
-
-export default Parent;
+export default App;
