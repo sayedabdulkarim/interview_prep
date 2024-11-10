0184f859b3c514b2a6a618682a220636e2304eab
Sayed Abdul <sakarim9124@gmail.com>
2024-11-10 23:23:27 +0530

    updated

--- react_interview/boilerplate/react_18th_v/src/App.js
+++ react_interview/boilerplate/react_18th_v/src/App.js
@@ -1,7 +1,49 @@
-import React from "react";
+import React, { Component, useState } from "react";
+
+class ErrorBoundary extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      hasError: false,
+    };
+  }
+
+  static getDerivedStateFromError() {
+    return { hasError: true };
+  }
+
+  componentDidCatch(error, info) {
+    console.log({ error, info }, " error from CDC");
+  }
+
+  render() {
+    return this.state.hasError ? (
+      <h1>Something went wrng</h1>
+    ) : (
+      this.props.children
+    );
+  }
+}
+
+const Button = () => {
+  const [forcedError, setForcedError] = useState(false);
+
+  if (forcedError) {
+    throw new Error("Somethin went wromg,.");
+  }
+
+  return <button onClick={() => setForcedError(true)}>Error</button>;
+};
 
 const App = () => {
-  return <div>App</div>;
+  return (
+    <div>
+      <h1>Main Component</h1>
+      <ErrorBoundary>
+        <Button />
+      </ErrorBoundary>
+    </div>
+  );
 };
 
 export default App;
