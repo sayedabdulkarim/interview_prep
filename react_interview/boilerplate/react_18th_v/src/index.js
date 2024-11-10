e4c8989b76e270ce48582487469f327a741f39a1
Sayed Abdul <sakarim9124@gmail.com>
2024-11-10 18:21:16 +0530

    updated

--- react_interview/boilerplate/react_18th_v/src/index.js
+++ react_interview/boilerplate/react_18th_v/src/index.js
@@ -1,80 +1,17 @@
-// import React from "react";
-// import ReactDOM from "react-dom/client";
-// import "./index.css";
-// import App from "./App";
-// import reportWebVitals from "./reportWebVitals";
-
-// const root = ReactDOM.createRoot(document.getElementById("root"));
-// root.render(
-//   // <React.StrictMode>
-//   <App />
-//   // </React.StrictMode>
-// );
-
-// // If you want to start measuring performance in your app, pass a function
-// // to log results (for example: reportWebVitals(console.log))
-// // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-// reportWebVitals();
-
 import React from "react";
 import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
 
-let stateValues = [];
-let root = null;
-let stateIndex = 0;
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  // <React.StrictMode>
+  <App />
+  // </React.StrictMode>
+);
 
-const useState = (val) => {
-  const currentIndex = stateIndex;
-  stateValues[currentIndex] = stateValues[currentIndex] ?? val;
-
-  const setVal = (newVal) => {
-    stateValues[currentIndex] = newVal;
-    renderApp();
-  };
-
-  stateIndex++;
-  return [stateValues[currentIndex], setVal];
-};
-
-const App = () => {
-  stateIndex = 0;
-
-  const [countOne, setCountOne] = useState(1);
-  const [countTwo, setCountTwo] = useState(2);
-  const [countThree, setCountThree] = useState(-1);
-
-  return (
-    <div>
-      <h1>Count : {countOne}</h1>
-      <button onClick={() => setCountOne(countOne + 1)}>ADD</button>
-      <button onClick={() => setCountOne(countOne - 1)}>MINUS</button>
-      <br />
-      <br />
-      <hr />
-      <h1>Count : {countTwo}</h1>
-      <button onClick={() => setCountTwo(countTwo + 1)}>ADD</button>
-      <button onClick={() => setCountTwo(countTwo - 1)}>MINUS</button>
-      <br />
-      <br />
-      <hr />
-      <h1>Count : {countThree}</h1>
-      <button onClick={() => setCountThree(countThree + 1)}>ADD</button>
-      <button onClick={() => setCountThree(countThree - 1)}>MINUS</button>
-      <br />
-      <br />
-      <hr />
-    </div>
-  );
-};
-
-const renderApp = () => {
-  if (!root) {
-    root = ReactDOM.createRoot(document.getElementById("root"));
-  }
-  root.render(
-    // <React.StrictMode>
-    <App />
-    // </React.StrictMode>
-  );
-};
-renderApp();
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
