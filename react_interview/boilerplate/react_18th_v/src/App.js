c8355400aac863f1c87baef7d5e0d09ee0402ce6
Sayed Abdul <sakarim9124@gmail.com>
2024-11-16 00:55:50 +0530

    updated

--- react_interview/boilerplate/react_18th_v/src/App.js
+++ react_interview/boilerplate/react_18th_v/src/App.js
@@ -3,17 +3,25 @@ import React, { useCallback, useEffect, useState } from "react";
 const App = () => {
   const [seconds, setSeconds] = useState(10);
   const [minutes, setMinutes] = useState(2);
+  const [hours, setHours] = useState(1);
 
   const timer = useCallback(() => {
     if (seconds > 0) {
       setSeconds((prev) => prev - 1);
-    } else if (seconds === 0) {
-      if (minutes > 0) {
-        setMinutes((prev) => prev - 1);
-        setSeconds(59);
-      }
+    } else if (seconds === 0 && minutes >= 1) {
+      setMinutes((prev) => prev - 1);
+      setSeconds(59);
     }
-  }, [minutes, seconds]);
+    // else if (seconds === 0 && minutes === 0 && hours === 0) {
+    //   setSeconds(0);
+    //   setMinutes(0);
+    //   setHours(0);
+    // } else {
+    //   setHours((prev) => prev - 1);
+    //   setSeconds(59);
+    //   setMinutes(59);
+    // }
+  }, [minutes, seconds, hours]);
 
   useEffect(() => {
     const handleTimer = setInterval(() => {
@@ -26,7 +34,7 @@ const App = () => {
   return (
     <div>
       <h1>
-        {seconds} : {minutes}
+        {hours} : {minutes} : {seconds}
       </h1>
     </div>
   );
