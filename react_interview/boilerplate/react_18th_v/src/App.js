c7bbb78f57942d4a420afdc6444a88b8dc4dbd23
Sayed Abdul <sakarim9124@gmail.com>
2024-11-16 00:22:51 +0530

    updated

--- react_interview/boilerplate/react_18th_v/src/App.js
+++ react_interview/boilerplate/react_18th_v/src/App.js
@@ -1,48 +1,33 @@
-import React, { Component, useState } from "react";
-
-class ErrorBoundary extends Component {
-  constructor(props) {
-    super();
-    this.state = {
-      hasError: false,
-    };
-  }
-
-  componentDidCatch(error, info) {
-    console.log({ error, info }, " CDC");
-  }
-
-  static getDerivedStateFromError(err) {
-    return { hasError: true };
-  }
-
-  render() {
-    return this.state.hasError ? (
-      <h1>Something went wrong</h1>
-    ) : (
-      this.props.children
-    );
-  }
-}
-
-const ButtonThatBreaks = () => {
-  const [forceError, setForceError] = useState(false);
-
-  //
-  if (forceError) {
-    throw new Error("Error from Button component.");
-  }
-
-  return <button onClick={() => setForceError(true)}>Click Error</button>;
-};
+import React, { useCallback, useEffect, useState } from "react";
 
 const App = () => {
+  const [seconds, setSeconds] = useState(10);
+  const [minutes, setMinutes] = useState(2);
+
+  const timer = useCallback(() => {
+    if (seconds > 0) {
+      setSeconds((prev) => prev - 1);
+    } else if (seconds === 0) {
+      if (minutes > 0) {
+        setMinutes((prev) => prev - 1);
+        setSeconds(59);
+      }
+    }
+  }, [minutes, seconds]);
+
+  useEffect(() => {
+    const handleTimer = setInterval(() => {
+      timer();
+    }, 1000);
+
+    return () => clearInterval(handleTimer);
+  }, [timer]);
+
   return (
     <div>
-      <h1>Main Component</h1>
-      <ErrorBoundary>
-        <ButtonThatBreaks />
-      </ErrorBoundary>
+      <h1>
+        {seconds} : {minutes}
+      </h1>
     </div>
   );
 };
