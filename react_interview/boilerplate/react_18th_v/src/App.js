0cde0492423c3a4a8840eef6d75896905e02c986
Sayed Abdul <sakarim9124@gmail.com>
2024-11-18 01:09:08 +0530

    updated

--- react_interview/boilerplate/react_18th_v/src/App.js
+++ react_interview/boilerplate/react_18th_v/src/App.js
@@ -1,41 +1,49 @@
-import React, { useCallback, useEffect, useState } from "react";
+import React, { useState } from "react";
+
+const useInput = (initialVal) => {
+  const [inpVal, setInpVal] = useState("");
+
+  const handleChange = (e) => {
+    const val = e.target.value;
+    setInpVal(val);
+  };
+
+  const handleReset = (e) => {
+    setInpVal(initialVal);
+  };
+
+  return [inpVal, handleChange, handleReset];
+};
 
 const App = () => {
-  const [seconds, setSeconds] = useState(10);
-  const [minutes, setMinutes] = useState(2);
-  const [hours, setHours] = useState(1);
+  const [firstName, handleFirstName, handleResetFirstName] = useInput("");
+  const [lastName, handleLastName, handleResetLastName] = useInput("");
 
-  const timer = useCallback(() => {
-    if (seconds > 0) {
-      setSeconds((prev) => prev - 1);
-    } else if (seconds === 0 && minutes >= 1) {
-      setMinutes((prev) => prev - 1);
-      setSeconds(59);
-    }
-    // else if (seconds === 0 && minutes === 0 && hours === 0) {
-    //   setSeconds(0);
-    //   setMinutes(0);
-    //   setHours(0);
-    // } else {
-    //   setHours((prev) => prev - 1);
-    //   setSeconds(59);
-    //   setMinutes(59);
-    // }
-  }, [minutes, seconds, hours]);
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log({ firstName, lastName }, " submitted");
+  };
 
-  useEffect(() => {
-    const handleTimer = setInterval(() => {
-      timer();
-    }, 1000);
-
-    return () => clearInterval(handleTimer);
-  }, [timer]);
+  const handleReset = () => {
+    handleResetFirstName();
+    handleResetLastName();
+  };
 
   return (
     <div>
-      <h1>
-        {hours} : {minutes} : {seconds}
-      </h1>
+      <form onSubmit={handleSubmit}>
+        <input
+          value={firstName}
+          onChange={handleFirstName}
+          placeholder="First Name"
+        />
+        <input
+          value={lastName}
+          onChange={handleLastName}
+          placeholder="Last Name"
+        />
+        <button type="submit">SUBMIT</button>
+      </form>
     </div>
   );
 };
