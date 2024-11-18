de2c6f5859e346227859caecb5e1e707071eb519
Sayed Abdul <sakarim9124@gmail.com>
2024-11-18 21:55:56 +0530

    updated

--- react_interview/boilerplate/react_18th_v/src/App.js
+++ react_interview/boilerplate/react_18th_v/src/App.js
@@ -1,79 +1,54 @@
-import React, { memo, useCallback, useMemo, useState } from "react";
+import React, { useState } from "react";
 
-const Parent = () => {
-  const [count, setCount] = useState(0);
-  const [countParent, setCountParent] = useState(0);
-  //memo
-  const [numbers, setNumbers] = useState([1, 2, 3]);
-
-  const handlClick = useCallback(() => {
-    setCount((prev) => prev + 1);
-  }, []);
-
-  // const handlClick = () => {
-  //   setCount((prev) => prev + 1);
-  // };
-  const handlClickParent = () => {
-    setCountParent((prev) => prev + 1);
-  };
-
-  //memo
-  // const maxNumber = () => {
-  //   console.log("Calculating max number...");
-  //   return Math.max(...numbers);
-  // };
-  const maxNumber = useMemo(() => {
-    console.log("Calculating max number...");
-    return Math.max(...numbers);
-  }, [numbers]);
-
-  console.log(" parent render ");
-
+const App = () => {
   return (
     <div>
-      <h1>Parent {countParent}</h1>
-      <button onClick={handlClickParent}>HANDLECLIKCPARENT</button>
-      <br />
-      <br />
-      <ChildOne count={count} handlClick={handlClick} />
-      <br />
-      <br />
-      <ChildTwo />
-      {/*  */}
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      {/* <h1>Max Number: {maxNumber()}</h1> */}
-      <h1>Max Number: {maxNumber}</h1>
-      {/* 
-        the maximum number is only recalculated when the numbers array changes. This means that clicking the "Change Other State" button won't trigger a recalculation, */}
+      <RenderProps
+        render={(count, setCount) => (
+          <Render1Props count={count} setCount={setCount} />
+        )}
+      />
 
-      {JSON.stringify(numbers, null, 4)}
-      <button
-        onClick={() => setNumbers([...numbers, Math.floor(Math.random() * 10)])}
-      >
-        Add Random Number
-      </button>
+      <br />
+      <br />
+      <br />
+      <br />
+
+      <RenderProps
+        render={(count, setCount) => (
+          <Render2Props count={count} setCount={setCount} />
+        )}
+      />
     </div>
   );
 };
 
-const ChildOne = memo(({ count, handlClick }) => {
-  console.log("childOne render");
+const Render1Props = ({ count, setCount }) => {
   return (
     <>
-      <h1>Child One - {count}</h1>
-      <button onClick={handlClick}>HANDLE CLIKC</button>
+      <h1>Render 1 Props : {count}</h1>
+      <button onClick={() => setCount()}>Counter 1</button>
     </>
   );
-});
+};
 
-const ChildTwo = memo(() => {
-  console.log("childTwo render");
-  return <h1>Child Two </h1>;
-});
+const Render2Props = ({ count, setCount }) => {
+  return (
+    <>
+      <h1>Render 2 Props: {count}</h1>
+      <button onClick={() => setCount()}>Counter 1</button>
+    </>
+  );
+};
 
-export default Parent;
+const RenderProps = (props) => {
+  const [count, setCount] = useState(0);
+
+  const handleCount = () => {
+    setCount((prev) => prev + 1);
+  };
+
+  return <div>{props.render(count, handleCount)}</div>;
+};
+
+export default App;
