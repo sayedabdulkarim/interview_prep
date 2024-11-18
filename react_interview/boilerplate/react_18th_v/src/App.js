6b504cb27bcc594387b2690d4b95321ffdf34165
Sayed Abdul <sakarim9124@gmail.com>
2024-11-18 17:59:23 +0530

    updated

--- react_interview/boilerplate/react_18th_v/src/App.js
+++ react_interview/boilerplate/react_18th_v/src/App.js
@@ -1,8 +1,10 @@
-import React, { memo, useCallback, useState } from "react";
+import React, { memo, useCallback, useMemo, useState } from "react";
 
 const Parent = () => {
   const [count, setCount] = useState(0);
   const [countParent, setCountParent] = useState(0);
+  //memo
+  const [numbers, setNumbers] = useState([1, 2, 3]);
 
   const handlClick = useCallback(() => {
     setCount((prev) => prev + 1);
@@ -14,6 +16,16 @@ const Parent = () => {
   const handlClickParent = () => {
     setCountParent((prev) => prev + 1);
   };
+
+  //memo
+  // const maxNumber = () => {
+  //   console.log("Calculating max number...");
+  //   return Math.max(...numbers);
+  // };
+  const maxNumber = useMemo(() => {
+    console.log("Calculating max number...");
+    return Math.max(...numbers);
+  }, [numbers]);
 
   console.log(" parent render ");
 
@@ -27,6 +39,24 @@ const Parent = () => {
       <br />
       <br />
       <ChildTwo />
+      {/*  */}
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      {/* <h1>Max Number: {maxNumber()}</h1> */}
+      <h1>Max Number: {maxNumber}</h1>
+      {/* 
+        the maximum number is only recalculated when the numbers array changes. This means that clicking the "Change Other State" button won't trigger a recalculation, */}
+
+      {JSON.stringify(numbers, null, 4)}
+      <button
+        onClick={() => setNumbers([...numbers, Math.floor(Math.random() * 10)])}
+      >
+        Add Random Number
+      </button>
     </div>
   );
 };
