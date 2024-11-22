ed867a43442c0fe6b84eddcf30271c18ccb539f3
Sayed Abdul <sakarim9124@gmail.com>
2024-11-22 13:44:32 +0530

    updated

--- react_interview/boilerplate/react_18th_v/src/App.js
+++ react_interview/boilerplate/react_18th_v/src/App.js
@@ -3,7 +3,17 @@ import React, { useEffect, useState } from "react";
 const App = () => {
   const [seconds, setSeconds] = useState(10);
   const [minutes, setMinutes] = useState(2);
+  const [hours, setHours] = useState(1);
 
+  //
+  // const handleTimer = () => {
+  //   if (seconds > 0) {
+  //     setSeconds((prev) => prev - 1);
+  //   } else if (seconds === 0 && minutes > 0) {
+  //     setMinutes((prev) => prev - 1);
+  //     setSeconds(10);
+  //   }
+  // };
   //
   const handleTimer = () => {
     if (seconds > 0) {
@@ -11,6 +21,14 @@ const App = () => {
     } else if (seconds === 0 && minutes > 0) {
       setMinutes((prev) => prev - 1);
       setSeconds(10);
+    } else if (seconds === 0 && minutes === 0 && hours === 0) {
+      setSeconds(0);
+      setMinutes(0);
+      setHours(0);
+    } else {
+      setSeconds(10);
+      setMinutes(10);
+      setHours((prev) => prev - 1);
     }
   };
 
@@ -25,7 +43,7 @@ const App = () => {
   return (
     <div>
       <h1>
-        {minutes} : {seconds}
+        {hours} : {minutes} : {seconds}
       </h1>
     </div>
   );
