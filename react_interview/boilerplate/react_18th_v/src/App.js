e52b8422af58d60d64a263a85aeddc343e8cce60
Sayed Abdul <sakarim9124@gmail.com>
2024-11-22 12:51:47 +0530

    updated

--- react_interview/boilerplate/react_18th_v/src/App.js
+++ react_interview/boilerplate/react_18th_v/src/App.js
@@ -1,54 +1,32 @@
-import React, { useState } from "react";
+import React, { forwardRef, useImperativeHandle, useRef } from "react";
 
 const App = () => {
+  const childRef = useRef();
+
+  const handleClick = () => {
+    if (childRef.current) {
+      // console.log(childRef);
+      childRef.current.childFunc();
+    }
+  };
   return (
     <div>
-      <RenderProps
-        render={(count, setCount) => (
-          <Render1Props count={count} setCount={setCount} />
-        )}
-      />
-
-      <br />
-      <br />
-      <br />
-      <br />
-
-      <RenderProps
-        render={(count, setCount) => (
-          <Render2Props count={count} setCount={setCount} />
-        )}
-      />
+      <h1>Paremt component</h1>
+      <button onClick={handleClick}>Child Clikc</button>
+      <ChildComponent ref={childRef} />
     </div>
   );
 };
 
-const Render1Props = ({ count, setCount }) => {
-  return (
-    <>
-      <h1>Render 1 Props : {count}</h1>
-      <button onClick={() => setCount()}>Counter 1</button>
-    </>
-  );
-};
-
-const Render2Props = ({ count, setCount }) => {
-  return (
-    <>
-      <h1>Render 2 Props: {count}</h1>
-      <button onClick={() => setCount()}>Counter 1</button>
-    </>
-  );
-};
-
-const RenderProps = (props) => {
-  const [count, setCount] = useState(0);
-
-  const handleCount = () => {
-    setCount((prev) => prev + 1);
-  };
-
-  return <div>{props.render(count, handleCount)}</div>;
-};
+const ChildComponent = forwardRef((_, ref) => {
+  useImperativeHandle(ref, () => {
+    return {
+      childFunc() {
+        console.log("chld clicked");
+      },
+    };
+  });
+  return <h1>Child component</h1>;
+});
 
 export default App;
