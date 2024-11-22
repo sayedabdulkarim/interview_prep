48a49bc72129201638acdb5bf63606fbdfd1e1f2
Sayed Abdul <sakarim9124@gmail.com>
2024-11-22 23:54:02 +0530

    updated

--- react_interview/boilerplate/react_18th_v/src/App.js
+++ react_interview/boilerplate/react_18th_v/src/App.js
@@ -1,58 +1,49 @@
-import React, { memo, useCallback, useMemo, useState } from "react";
+import React, { useState } from "react";
 
-const Parent = () => {
-  const arr = [3, 3, 4, 5, 21, 2, 42, 343, 424, 12, 3, 56, 7, 8, 8];
-  const [count, setCount] = useState(0);
-  const [parentToggle, setParentToggle] = useState(false);
-  const [maxNum, setMaxNum] = useState(arr);
-  //
-  const handleCount = useCallback(() => {
-    setCount((prev) => prev + 1);
-  }, []);
-
-  const getMax = useMemo(() => {
-    console.log("getmax called");
-    return Math.max(...maxNum);
-  }, [maxNum]);
-
-  console.log(" render Parent");
-
+const App = () => {
   return (
     <div>
-      {/* <h1>Parent {getMax()}</h1> */}
-      <h1>Parent {getMax}</h1>
-      <h3>{parentToggle ? "parent toggle true" : "parent toggle false "} </h3>
-      {JSON.stringify(maxNum, null, 2)}
-      {/* <button onClick={() => setMaxNum(...maxNum, Math.round(Math.random))}> */}
-      <button onClick={() => setMaxNum([...maxNum, Math.round(Math.random)])}>
-        Push
-      </button>
-      <button onClick={() => setParentToggle((prev) => !prev)}>
-        Parent toggle
-      </button>
+      <RenderProps
+        render={(count, handleCount) => (
+          <Render1Props count={count} handleCount={handleCount} />
+        )}
+      />
       <br />
       <br />
-      <ChildOne count={count} handleCount={handleCount} />
       <br />
-      <br />
-      <ChildTwo />
+      <RenderProps
+        render={(count, handleCount) => (
+          <Render1Props count={count} handleCount={handleCount} />
+        )}
+      />
     </div>
   );
 };
 
-const ChildOne = memo(({ count, handleCount }) => {
-  console.log(" render child one ");
+const RenderProps = ({ render }) => {
+  const [count, setCount] = useState(0);
+
+  const handleCount = () => setCount((prev) => prev + 1);
+
+  return render(count, handleCount);
+};
+
+const Render1Props = ({ count, handleCount }) => {
   return (
     <div>
-      <h1>Child One - {count}</h1>
-      <button onClick={handleCount}>Click</button>
+      <h1>Render1Props : {count}</h1>
+      <button onClick={handleCount}>CLick</button>
     </div>
   );
-});
+};
 
-const ChildTwo = memo(() => {
-  console.log("render Child Two");
-  return <h1>Child Two</h1>;
-});
+const Render2Props = ({ count, handleCount }) => {
+  return (
+    <div>
+      <h1>Render2Props : {count}</h1>
+      <button onClick={handleCount}>CLick</button>
+    </div>
+  );
+};
 
-export default Parent;
+export default App;
