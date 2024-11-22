7fdb2201b4b002534b96791c12fca26953e55735
Sayed Abdul <sakarim9124@gmail.com>
2024-11-23 00:09:17 +0530

    updated

--- react_interview/boilerplate/react_18th_v/src/App.js
+++ react_interview/boilerplate/react_18th_v/src/App.js
@@ -1,47 +1,42 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
+
+const useFetch = (url) => {
+  const [loading, setLoading] = useState(false);
+  const [data, setData] = useState(null);
+  const [error, setError] = useState(false);
+
+  useEffect(() => {
+    setLoading(true);
+    fetch(url)
+      .then((res) => res.json())
+      .then((res) => {
+        console.log(res, " ress");
+        setLoading(false);
+        setData(res);
+      })
+      .catch((err) => {
+        setError(err);
+        setLoading(false);
+      });
+  }, [url]);
+
+  // return [loading, data, error];
+  return { loading, data, error };
+};
 
 const App = () => {
+  const {
+    loading: isLoading,
+    error: isError,
+    data: dummyData,
+  } = useFetch("https://jsonplaceholder.typicode.com/posts");
+
   return (
     <div>
-      <RenderProps
-        render={(count, handleCount) => (
-          <Render1Props count={count} handleCount={handleCount} />
-        )}
-      />
-      <br />
-      <br />
-      <br />
-      <RenderProps
-        render={(count, handleCount) => (
-          <Render1Props count={count} handleCount={handleCount} />
-        )}
-      />
-    </div>
-  );
-};
-
-const RenderProps = ({ render }) => {
-  const [count, setCount] = useState(0);
-
-  const handleCount = () => setCount((prev) => prev + 1);
-
-  return render(count, handleCount);
-};
-
-const Render1Props = ({ count, handleCount }) => {
-  return (
-    <div>
-      <h1>Render1Props : {count}</h1>
-      <button onClick={handleCount}>CLick</button>
-    </div>
-  );
-};
-
-const Render2Props = ({ count, handleCount }) => {
-  return (
-    <div>
-      <h1>Render2Props : {count}</h1>
-      <button onClick={handleCount}>CLick</button>
+      {isLoading && <h1>Loading....</h1>}
+      <h1>Hello</h1>
+      <pre>{JSON.stringify(dummyData, null, 2)}</pre>
+      <button onClick={() => console.log(dummyData, " dd")}>data</button>
     </div>
   );
 };
