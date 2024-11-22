14bc33761b717e0679fc781cca7ce350310808ca
Sayed Abdul <sakarim9124@gmail.com>
2024-11-22 23:32:36 +0530

    updated

--- react_interview/boilerplate/react_18th_v/src/App.js
+++ react_interview/boilerplate/react_18th_v/src/App.js
@@ -1,22 +1,45 @@
-import React, { useState } from "react";
+import React, { memo, useCallback, useState } from "react";
 
-const useInput = (initialVal) => {
-  const [val, setVal] = useState(initialVal);
-
+const Parent = () => {
+  const [count, setCount] = useState(0);
+  const [parentToggle, setParentToggle] = useState(false);
   //
-  const handleChange = (e) => {
-    setVal(e.target.value);
-  };
+  const handleCount = useCallback(() => {
+    setCount((prev) => prev + 1);
+  }, []);
 
-  const handleReset = (e) => {
-    setVal(initialVal);
-  };
+  console.log(" render Parent");
 
-  return [val, handleChange, handleReset];
+  return (
+    <div>
+      <h1>Parent</h1>
+      <h3>{parentToggle ? "parent toggle true" : "parent toggle false "} </h3>
+      <button onClick={() => setParentToggle((prev) => !prev)}>
+        Parent toggle
+      </button>
+      <br />
+      <br />
+      <ChildOne count={count} handleCount={handleCount} />
+      <br />
+      <br />
+      <ChildTwo />
+    </div>
+  );
 };
 
-const App = () => {
-  return <div>App</div>;
-};
+const ChildOne = memo(({ count, handleCount }) => {
+  console.log(" render child one ");
+  return (
+    <div>
+      <h1>Child One - {count}</h1>
+      <button onClick={handleCount}>Click</button>
+    </div>
+  );
+});
 
-export default App;
+const ChildTwo = memo(() => {
+  console.log("render Child Two");
+  return <h1>Child Two</h1>;
+});
+
+export default Parent;
