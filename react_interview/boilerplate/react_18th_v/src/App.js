a68e1a3842dbf2118da417629d7a7376d8b74e0a
Sayed Abdul <sakarim9124@gmail.com>
2024-11-24 23:48:22 +0530

    updated

--- react_interview/boilerplate/react_18th_v/src/App.js
+++ react_interview/boilerplate/react_18th_v/src/App.js
@@ -1,44 +1,31 @@
-import React, { useEffect, useState } from "react";
-
-const useFetch = (url) => {
-  const [loading, setLoading] = useState(false);
-  const [data, setData] = useState(null);
-  const [error, setError] = useState(false);
-
-  useEffect(() => {
-    setLoading(true);
-    fetch(url)
-      .then((res) => res.json())
-      .then((res) => {
-        console.log(res, " ress");
-        setLoading(false);
-        setData(res);
-      })
-      .catch((err) => {
-        setError(err);
-        setLoading(false);
-      });
-  }, [url]);
-
-  // return [loading, data, error];
-  return { loading, data, error };
-};
+import React, { forwardRef, useImperativeHandle, useRef } from "react";
 
 const App = () => {
-  const {
-    loading: isLoading,
-    error: isError,
-    data: dummyData,
-  } = useFetch("https://jsonplaceholder.typicode.com/posts");
+  const childRef = useRef();
 
+  const handleClick = () => {
+    if (childRef.current) {
+      childRef.current.childClick();
+    }
+  };
   return (
     <div>
-      {isLoading && <h1>Loading....</h1>}
-      <h1>Hello</h1>
-      <pre>{JSON.stringify(dummyData, null, 2)}</pre>
-      <button onClick={() => console.log(dummyData, " dd")}>data</button>
+      <h1>Parent</h1>
+      <button onClick={handleClick}>CLICK</button>
+      <ChildrenComp ref={childRef} />
     </div>
   );
 };
+
+const ChildrenComp = forwardRef((_, ref) => {
+  useImperativeHandle(ref, () => {
+    return {
+      childClick() {
+        console.log("clicked child");
+      },
+    };
+  });
+  return <h1>Chilld</h1>;
+});
 
 export default App;
