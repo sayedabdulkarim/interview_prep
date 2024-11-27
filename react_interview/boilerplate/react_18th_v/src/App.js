0628a4e082a7502fa62341d0fe4ef4f8cf2eda7f
Sayed Abdul <sakarim9124@gmail.com>
2024-11-28 00:16:27 +0530

    updated

--- react_interview/boilerplate/react_18th_v/src/App.js
+++ react_interview/boilerplate/react_18th_v/src/App.js
@@ -3,7 +3,8 @@ import React, { useEffect, useState } from "react";
 const App = () => {
   const [seconds, setSeconds] = useState(15);
   const [minutes, setMinutes] = useState(2);
-
+  const [hours, setHours] = useState(2);
+  const [isStart, setIsStart] = useState(false);
   //
   const handleTimer = () => {
     if (seconds > 0) {
@@ -11,22 +12,32 @@ const App = () => {
     } else if (seconds === 0 && minutes > 0) {
       setSeconds(10);
       setMinutes((prev) => prev - 1);
+    } else if (seconds === 0 && minutes === 0 && hours === 0) {
+      setSeconds(0);
+      setMinutes(0);
+      setHours(0);
+    } else {
+      setSeconds(10);
+      setMinutes(10);
+      setHours((prev) => prev - 1);
     }
   };
 
   useEffect(() => {
-    const timer = setInterval(() => {
-      handleTimer();
-    }, 1000);
-
-    return () => clearInterval(timer);
+    if (isStart) {
+      const timer = setInterval(() => {
+        handleTimer();
+      }, 1000);
+      return () => clearInterval(timer);
+    }
   });
 
   return (
     <div>
       <h1>
-        {seconds} : {minutes}
+        {hours} : {minutes} : {seconds}
       </h1>
+      <button onClick={() => setIsStart((prev) => !prev)}>TOGGLE</button>
     </div>
   );
 };
