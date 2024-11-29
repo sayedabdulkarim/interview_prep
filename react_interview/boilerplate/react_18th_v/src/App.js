1f65c8f868ccd6abb1c1aad5f4438df7950fd112
Sayed Abdul <sakarim9124@gmail.com>
2024-11-30 01:54:44 +0530

    updated

--- react_interview/boilerplate/react_18th_v/src/App.js
+++ react_interview/boilerplate/react_18th_v/src/App.js
@@ -1,11 +1,17 @@
-import React, { memo, useCallback, useState } from "react";
+import React, { memo, useCallback, useMemo, useState } from "react";
 
 const App = () => {
   //
   const [parentToggle, setParentToggle] = useState(false);
   const [counter, setCounter] = useState(0);
-
+  const [arr, setArr] = useState([2, 3, 1, 2, 11, 2, 1, 223, 4, 1, 3]);
   //
+
+  const getMax = useMemo(() => {
+    console.log("called getMax");
+    return Math.max(...arr);
+  }, [arr]);
+
   const handlePrentToggle = () => {
     setParentToggle((prev) => !prev);
   };
@@ -19,6 +25,8 @@ const App = () => {
   return (
     <div>
       <h1>Parent</h1>
+      {/* <h1>Max No: {getMax()}</h1> */}
+      <h1>Max No: {getMax}</h1>
       <button onClick={handlePrentToggle}>
         parent toggle {parentToggle ? " Yes" : " No"}
       </button>
