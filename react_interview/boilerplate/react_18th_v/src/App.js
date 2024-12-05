91afca05689428834515ac2d25b3f41ee1f190d2
Sayed Abdul <sakarim9124@gmail.com>
2024-12-05 23:22:28 +0530

    updated

--- react_interview/boilerplate/react_18th_v/src/App.js
+++ react_interview/boilerplate/react_18th_v/src/App.js
@@ -1,29 +1,4 @@
-import React, { Component } from "react";
-
-class ErrorBoundary extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      hasError: false,
-    };
-  }
-
-  componentDidCatch(error, info) {
-    console.log({ error, info }, " error from CDC");
-  }
-
-  static getDerivedStateFromError() {
-    return { hasError: true };
-  }
-
-  render() {
-    return this.state.hasError ? (
-      <h1>Somethog went wrong .. </h1>
-    ) : (
-      this.props.children
-    );
-  }
-}
+import React from "react";
 
 const App = () => {
   return <div>App</div>;
