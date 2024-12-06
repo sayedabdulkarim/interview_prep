f924cffa3417ba987a9e689b41c1d3a4335c3972
Sayed Abdul <sakarim9124@gmail.com>
2024-12-07 00:25:25 +0530

    updated

--- react_interview/boilerplate/react_18th_v/src/App.js
+++ react_interview/boilerplate/react_18th_v/src/App.js
@@ -1,7 +1,32 @@
-import React from "react";
+import React, { forwardRef, useImperativeHandle, useRef } from "react";
 
 const App = () => {
-  return <div>App</div>;
+  const childRef = useRef();
+
+  const handleClick = () => {
+    if (childRef.current) {
+      childRef.current.childClick();
+    }
+  };
+  return (
+    <div>
+      <h1>Parent</h1>
+      <button onClick={handleClick}>CLick</button>
+      <ChildComp ref={childRef} />
+    </div>
+  );
 };
+
+const ChildComp = forwardRef((_, ref) => {
+  useImperativeHandle(ref, () => {
+    return {
+      childClick() {
+        console.log("child Cliick");
+      },
+    };
+  });
+
+  return <h1>Child Component</h1>;
+});
 
 export default App;
