16733a92eb0ccf4957d5f5b3a4fd21ffd0f6fc28
Sayed Abdul <sakarim9124@gmail.com>
2024-12-07 00:40:00 +0530

    updated

--- react_interview/boilerplate/react_18th_v/src/index.js
+++ react_interview/boilerplate/react_18th_v/src/index.js
@@ -1,17 +1,77 @@
+// import React from "react";
+// import ReactDOM from "react-dom/client";
+// import "./index.css";
+// import App from "./App";
+// import reportWebVitals from "./reportWebVitals";
+
+// const root = ReactDOM.createRoot(document.getElementById("root"));
+// root.render(
+//   // <React.StrictMode>
+//   <App />
+//   // </React.StrictMode>
+// );
+
+// // If you want to start measuring performance in your app, pass a function
+// // to log results (for example: reportWebVitals(console.log))
+// // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+// reportWebVitals();
+
 import React from "react";
 import ReactDOM from "react-dom/client";
-import "./index.css";
-import App from "./App";
-import reportWebVitals from "./reportWebVitals";
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(
-  // <React.StrictMode>
-  <App />
-  // </React.StrictMode>
-);
+let root;
+let initialIndex = 0;
+let stateValues = [];
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+const useState = (initVal) => {
+  const currentIdx = initialIndex;
+  stateValues[currentIdx] = stateValues[currentIdx] ?? initVal;
+  initialIndex++;
+  const setVal = (val) => {
+    renderApp();
+    stateValues[currentIdx] = val;
+  };
+
+  return [stateValues[currentIdx], setVal];
+};
+
+const App = () => {
+  //
+  initialIndex = 0;
+  const [counterOne, setCounterOne] = useState(1);
+  const [counterTwo, setCounterTwo] = useState(0);
+  const [counterThree, setCounterThree] = useState(-1);
+
+  return (
+    <div>
+      <h1>{counterOne}</h1>
+      <button onClick={() => setCounterOne(counterOne + 1)}>ADD</button>
+      <button onClick={() => setCounterOne(counterOne - 1)}>MINUS</button>
+      <br />
+      <br />
+      <h1>{counterTwo}</h1>
+      <button onClick={() => setCounterTwo(counterTwo + 1)}>ADD</button>
+      <button onClick={() => setCounterTwo(counterTwo - 1)}>MINUS</button>
+      <br />
+      <br />
+      <h1>{counterThree}</h1>
+      <button onClick={() => setCounterThree(counterThree + 1)}>ADD</button>
+      <button onClick={() => setCounterThree(counterThree - 1)}>MINUS</button>
+      <br />
+      <br />
+    </div>
+  );
+};
+
+const renderApp = () => {
+  if (!root) {
+    root = ReactDOM.createRoot(document.getElementById("root"));
+  }
+  root.render(
+    // <React.StrictMode>
+    <App />
+    // </React.StrictMode>
+  );
+};
+
+renderApp();
