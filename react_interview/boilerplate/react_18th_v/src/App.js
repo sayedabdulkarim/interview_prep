4d9326621d04b16e94f19a40d80bcbd67369bac1
Sayed Abdul <sakarim9124@gmail.com>
2024-12-08 13:23:38 +0530

    updated

--- react_interview/boilerplate/react_18th_v/src/App.js
+++ react_interview/boilerplate/react_18th_v/src/App.js
@@ -1,59 +1,50 @@
-import React, { memo, useCallback, useMemo, useState } from "react";
+import React, { useState } from "react";
 
-const Parent = () => {
-  const [toggleParent, setToggleParent] = useState(false);
+const RenderProps = ({ render }) => {
   const [counter, setCounter] = useState(0);
-  const [arr, setArr] = useState([2, 1, 202, 12, 34, 234, 234, 11, 1]);
-  ///
 
-  // const maxVal = () => {
-  //   console.log("called maxVal");
-  //   return Math.max([...arr]);
-  // };
-
-  const maxVal = useMemo(() => {
-    console.log("called maxVal");
-    return Math.max([...arr]);
-  }, [arr]);
-
-  const handleCounter = useCallback(() => {
+  const handleCounter = () => {
     setCounter((prev) => prev + 1);
-  }, []);
-
-  const pushRandom = () => {
-    const num = Math.random();
-    setArr([...arr, num]);
   };
 
-  console.log("parent renders");
+  return <>{render(counter, handleCounter)}</>;
+};
+
+const CounterOne = ({ counter, handleCounter }) => {
   return (
     <div>
-      <h1>Parent - {maxVal}</h1>
-      <button onClick={() => setToggleParent((prev) => !prev)}>
-        Parent Toggle : {toggleParent ? "true" : "false"}
-      </button>
-      <button onClick={pushRandom}>PUSH TO MAX</button>
-      <pre>{JSON.stringify(arr, null, 2)}</pre>
-
-      <ChildOne counter={counter} handleCounter={handleCounter} />
-      <ChildTwo />
+      <h1>Counter One : {counter}</h1>
+      <button onClick={handleCounter}>handleCounter</button>
     </div>
   );
 };
 
-const ChildOne = memo(({ counter, handleCounter }) => {
-  console.log(" render child one");
+const CounterTwo = ({ counter, handleCounter }) => {
   return (
     <div>
-      <h1>ChildOne : {counter}</h1>
+      <h1>Counter Two : {counter}</h1>
       <button onClick={handleCounter}>handleCounter</button>
     </div>
   );
-});
+};
 
-const ChildTwo = memo(() => {
-  console.log(" render child two");
-  return <div>ChildTwo</div>;
-});
+const App = () => {
+  return (
+    <div>
+      <RenderProps
+        render={(counter, handleCounter) => (
+          <CounterOne counter={counter} handleCounter={handleCounter} />
+        )}
+      />
+      <br />
+      <br />
+      <RenderProps
+        render={(counter, handleCounter) => (
+          <CounterTwo counter={counter} handleCounter={handleCounter} />
+        )}
+      />
+    </div>
+  );
+};
 
-export default Parent;
+export default App;
