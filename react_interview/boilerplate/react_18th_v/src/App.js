d0a164a86bf627ec64e1e5981c2732076a7b9ad5
Sayed Abdul <sakarim9124@gmail.com>
2024-12-15 02:20:40 +0530

    updated

--- react_interview/boilerplate/react_18th_v/src/App.js
+++ react_interview/boilerplate/react_18th_v/src/App.js
@@ -1,53 +1,61 @@
-import React, { useState } from "react";
-
-const useInput = (val) => {
-  const [inpVal, setInpVal] = useState(val);
-
-  const handleChange = (e) => {
-    setInpVal(e.target.value);
-  };
-
-  const handleReset = (_) => {
-    setInpVal(val);
-  };
-
-  return { inpVal, handleChange, handleReset };
-};
+import React, { memo, useCallback, useMemo, useState } from "react";
 
 const App = () => {
-  const {
-    inpVal: firstName,
-    handleChange: handleFirstNameChange,
-    handleReset: handleFirstNameReset,
-  } = useInput("");
-  const {
-    inpVal: lastName,
-    handleChange: handleLastNameChange,
-    handleReset: handleLastNameReset,
-  } = useInput("");
+  //
+  const [toggle, setToggle] = useState(false);
+  const [counter, setCounter] = useState(0);
+  const [arr, setArr] = useState([
+    1, 2, 3, 1, 3, 4, 5, 22, 41, 333, 1, 2, 1, 3, 4,
+  ]);
+  //
+  const handleCounter = useCallback(() => {
+    setCounter((prev) => prev + 1);
+  }, []);
 
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    console.log({ firstName, lastName }, " submitted");
-  };
+  const getMax = useMemo(() => {
+    console.log(" get Max called");
+    return Math.max(...arr);
+  }, [arr]);
 
-  const handleReset = () => {
-    handleFirstNameReset("");
-    handleLastNameReset("");
-  };
+  // const pushRandom = () => {
+  //   const rand = Math.random();
+  //   return setArr.push(rand);
+  // };
 
+  console.log("render parent");
   return (
     <div>
-      <form onSubmit={handleSubmit}>
-        <input type="text" onChange={handleFirstNameChange} value={firstName} />
-        <input type="text" onChange={handleLastNameChange} value={lastName} />
-        <button type="submit">SUBMIT</button>
-        <button type="button" onClick={handleReset}>
-          RESET
-        </button>
-      </form>
+      <h1>Parent {getMax}</h1>
+      <pre>{JSON.stringify(arr, null, 2)}</pre>
+      <button
+        onClick={() => setToggle((prev) => !prev)}
+      >{`Toggle - ${toggle}`}</button>
+      {/* <button onClick={() => pushRandom()}>PUSH MAX</button> */}
+      <button onClick={() => setArr((prev) => [...prev, Math.random()])}>
+        PUSH MAX
+      </button>
+
+      <ComponentOne counter={counter} handleCounter={handleCounter} />
+      <br />
+      <br />
+      <ComponentTwo />
     </div>
   );
 };
+
+const ComponentOne = memo(({ counter, handleCounter }) => {
+  console.log("render component one");
+  return (
+    <div>
+      <h1>Counter One : {counter}</h1>
+      <button onClick={handleCounter}>handleCounter</button>
+    </div>
+  );
+});
+
+const ComponentTwo = memo(() => {
+  console.log("render component two");
+  return <div>App</div>;
+});
 
 export default App;
