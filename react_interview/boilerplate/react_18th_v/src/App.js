ac67b6015430cd91724fffe8e8b4aea0236f3d4c
Sayed Abdul <sakarim9124@gmail.com>
2024-12-21 12:11:29 +0530

    updated

--- react_interview/boilerplate/react_18th_v/src/App.js
+++ react_interview/boilerplate/react_18th_v/src/App.js
@@ -1,40 +1,7 @@
-import React, { useEffect, useState } from "react";
-
-const useFetch = (url) => {
-  const [data, setData] = useState(null);
-  const [error, setError] = useState(null);
-  const [loading, setLoading] = useState(false);
-
-  useEffect(() => {
-    setLoading(true);
-    fetch(url)
-      .then((res) => res.json())
-      .then((res) => {
-        console.log(res, " dd");
-        return setData(res);
-      })
-      .catch((err) => setError(err))
-      .finally(() => {
-        setError(null);
-        setLoading(false);
-      });
-  }, [url]);
-
-  return [data, loading, error];
-};
+import React from "react";
 
 const App = () => {
-  const [data, loading, error] = useFetch(
-    "https://jsonplaceholder.typicode.com/albums"
-  );
-
-  if (loading) return <h1>LOadin....</h1>;
-  if (error) return <pre>{JSON.stringify(error, null, 2)}</pre>;
-  return (
-    <div>
-      <pre>{JSON.stringify(data, null, 2)}</pre>
-    </div>
-  );
+  return <div>App</div>;
 };
 
 export default App;
