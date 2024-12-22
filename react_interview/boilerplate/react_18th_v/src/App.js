6815314feb62dd032db553fb3aa5da4c9553d38c
Sayed Abdul <sakarim9124@gmail.com>
2024-12-22 12:09:15 +0530

    updated

--- react_interview/boilerplate/react_18th_v/src/App.js
+++ react_interview/boilerplate/react_18th_v/src/App.js
@@ -1,51 +1,40 @@
-import React, { Component, useState } from "react";
-
-class ErrorBoundary extends Component {
-  constructor(props) {
-    super(props);
-
-    this.state = { hasError: false };
-  }
-
-  componentDidCatch(error, info) {
-    console.log({ error, info }, " from CDC");
-  }
-
-  static getDerivedStateFromError(err) {
-    return { hasError: true };
-  }
-
-  render() {
-    return this.state.hasError ? (
-      <h1>Something went wrong</h1>
-    ) : (
-      this.props.children
-    );
-  }
-}
+import React, { useCallback, useEffect, useState } from "react";
 
 const App = () => {
+  const [seconds, setSeconds] = useState(10);
+  const [minutes, setMinutes] = useState(2);
+  const [hours, setHours] = useState(1);
+
+  //
+  const handleTimer = useCallback(() => {
+    if (seconds > 0) {
+      setSeconds((prev) => prev - 1);
+    } else if (minutes > 0) {
+      setSeconds(59);
+      setMinutes((prev) => prev - 1);
+    } else if (seconds === 0 && minutes === 0 && hours === 0) {
+      setSeconds(0);
+      setMinutes(0);
+      setHours(0);
+    } else {
+      setSeconds(59);
+      setMinutes(59);
+      setHours((prev) => prev - 1);
+    }
+  }, [hours, minutes, seconds]);
+
+  useEffect(() => {
+    const timer = setInterval(handleTimer, 1000);
+
+    return () => clearInterval(timer);
+  }, [handleTimer]);
+
   return (
     <div>
-      <h1>HOME</h1>
-      <h1>ABOUT</h1>
-      <ErrorBoundary>
-        <ButtonComponent />
-      </ErrorBoundary>
-    </div>
-  );
-};
-
-const ButtonComponent = () => {
-  const [forceError, setForceError] = useState(false);
-
-  if (forceError) {
-    throw new Error("Error from Button Component");
-  }
-
-  return (
-    <div>
-      <button onClick={() => setForceError(true)}>FORCE ERROR </button>
+      <button>TOGGLE</button>
+      <h1>
+        {seconds} : {minutes} : {hours}
+      </h1>
     </div>
   );
 };
