ebfb69dc209218596e0ded93bb64b893ab11665b
Sayed Abdul <sakarim9124@gmail.com>
2025-01-02 10:33:33 +0530

    updated

--- react_interview/boilerplate/react_18th_v/src/App.js
+++ react_interview/boilerplate/react_18th_v/src/App.js
@@ -1,51 +1,47 @@
-import React, { Component } from "react";
-
-class ErrorBoundary extends Component {
-  constructor(props) {
-    super(props);
-    this.state = { hasError: false };
-  }
-
-  componentDidCatch(error, info) {
-    console.error({ error, info }, " error from componentDidCatch");
-  }
-
-  static getDerivedStateFromError(error) {
-    return { hasError: true };
-  }
-
-  render() {
-    return this.state.hasError ? (
-      <h1>Something went wrong</h1>
-    ) : (
-      this.props.children
-    );
-  }
-}
+import React, { useEffect } from "react";
 
 const App = () => {
+  const [seconds, setSeconds] = React.useState(10);
+  const [minutes, setMinutes] = React.useState(10);
+  const [hours, setHours] = React.useState(1);
+  const [isStart, setIsStart] = React.useState(false);
+
+  const timer = () => {
+    if (seconds >= 0) {
+      setSeconds((prev) => prev - 1);
+    } else if (minutes >= 0) {
+      setSeconds(59);
+      setMinutes((prev) => prev - 1);
+    } else if (seconds === 0 && minutes === 0 && hours === 0) {
+      setSeconds(0);
+      setMinutes(0);
+      setHours(0);
+    } else {
+      setSeconds(59);
+      setMinutes(59);
+      setHours((prev) => prev - 1);
+    }
+  };
+
+  useEffect(() => {
+    const handleTimer = timer();
+
+    setInterval(handleTimer, 1000);
+
+    return () => {
+      clearInterval(handleTimer);
+    };
+  });
+
   return (
     <div>
-      <h1>Parent Component</h1>
-      <h1>Home Component</h1>
-      <ErrorBoundary>
-        <ButtonComp />
-      </ErrorBoundary>
-    </div>
-  );
-};
-
-const ButtonComp = () => {
-  const [forceError, setForceError] = React.useState(false);
-
-  if (forceError) {
-    throw new Error("Error break");
-  }
-
-  return (
-    <div>
-      <h1>Button Component</h1>
-      <button onClick={() => setForceError(true)}>Click</button>
+      <h1>Timer</h1>
+      <h2>
+        {hours} : {minutes} : {seconds}
+      </h2>
+      <button onClick={() => setIsStart(!isStart)}>
+        {isStart ? "Stop" : "Start"}
+      </button>
     </div>
   );
 };
