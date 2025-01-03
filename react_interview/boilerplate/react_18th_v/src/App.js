fc482adbd3cf12a2481c6eebb8f7fab59c8e5c0a
Sayed Abdul <sakarim9124@gmail.com>
2025-01-03 10:23:17 +0530

    updated

--- react_interview/boilerplate/react_18th_v/src/App.js
+++ react_interview/boilerplate/react_18th_v/src/App.js
@@ -1,17 +1,17 @@
 import React, { useEffect } from "react";
 
 const App = () => {
-  const [seconds, setSeconds] = React.useState(10);
-  const [minutes, setMinutes] = React.useState(10);
-  const [hours, setHours] = React.useState(1);
+  const [seconds, setSeconds] = React.useState(0);
+  const [minutes, setMinutes] = React.useState(0);
+  const [hours, setHours] = React.useState(0);
   const [isStart, setIsStart] = React.useState(false);
 
   const timer = () => {
     if (seconds >= 0) {
-      setSeconds((prev) => prev - 1);
+      setSeconds((prev) => prev + 1);
     } else if (minutes >= 0) {
-      setSeconds(59);
-      setMinutes((prev) => prev - 1);
+      setSeconds((prev) => prev + 1);
+      setMinutes((prev) => prev + 1);
     } else if (seconds === 0 && minutes === 0 && hours === 0) {
       setSeconds(0);
       setMinutes(0);
@@ -23,27 +23,9 @@ const App = () => {
     }
   };
 
-  useEffect(() => {
-    const handleTimer = timer();
+  useEffect(() => {});
 
-    setInterval(handleTimer, 1000);
-
-    return () => {
-      clearInterval(handleTimer);
-    };
-  });
-
-  return (
-    <div>
-      <h1>Timer</h1>
-      <h2>
-        {hours} : {minutes} : {seconds}
-      </h2>
-      <button onClick={() => setIsStart(!isStart)}>
-        {isStart ? "Stop" : "Start"}
-      </button>
-    </div>
-  );
+  return <div>App</div>;
 };
 
 export default App;
