0900b3a061ef7d415ec38a27f5e99fa93d11c4a9
Sayed Abdul <sakarim9124@gmail.com>
2025-01-07 08:42:18 +0530

    updated

--- react_interview/boilerplate/react_18th_v/src/App.js
+++ react_interview/boilerplate/react_18th_v/src/App.js
@@ -3,13 +3,6 @@ import React from "react";
 const App = () => {
   const [seconds, setSeconds] = React.useState(0);
   const [minutes, setMinutes] = React.useState(0);
-  const [hours, setHours] = React.useState(0);
-  const [isStart, setIsStart] = React.useState(false);
-
-  const timer = () => {
-    if (seconds >= 0) {
-    }
-  };
 
   return <div>App</div>;
 };
