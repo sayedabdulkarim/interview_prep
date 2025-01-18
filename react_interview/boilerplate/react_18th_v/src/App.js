078a0bfd576543bca7697d3368d22a66f137b003
Sayed Abdul <sakarim9124@gmail.com>
2025-01-18 21:20:13 +0530

    updated

--- react_interview/boilerplate/react_18th_v/src/App.js
+++ react_interview/boilerplate/react_18th_v/src/App.js
@@ -1,10 +1,55 @@
-import React from "react";
+import React, { Component, useState } from "react";
+
+class ErrorBoundaries extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      hasError: false,
+    };
+  }
+
+  componentDidCatch(err) {
+    console.log("Something went wrong.");
+  }
+
+  getDerivedStateFromError(err) {
+    return { hasError: true };
+  }
+
+  render() {
+    return (
+      <div>
+        {this.state.hasError ? "Something went wrong " : this.props.children}
+      </div>
+    );
+  }
+}
+
+const ButtonComp = () => {
+  const [isBreak, setIsBreak] = useState(false);
+
+  if (isBreak) {
+    throw new Error("Error break");
+  }
+
+  return (
+    <div>
+      <h1>ErrorBoundaries</h1>
+      <button onClick={() => setIsBreak(true)}>Error CLick</button>
+    </div>
+  );
+};
 
 const App = () => {
-  const [seconds, setSeconds] = React.useState(0);
-  const [minutes, setMinutes] = React.useState(0);
-
-  return <div>App</div>;
+  return (
+    <div>
+      <h1>App Component</h1>
+      <ErrorBoundaries>
+        <ButtonComp />
+      </ErrorBoundaries>
+    </div>
+  );
 };
 
 export default App;
