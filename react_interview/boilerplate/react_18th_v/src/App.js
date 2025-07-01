7aa64e9358a9ae8a4bc6245e297de75ae1d94ed2
Sayed Abdul <sakarim9124@gmail.com>
2025-07-02 01:45:33 +0530

    Refactor App component to implement useMemo for getMax and add handleInsert function for dynamic number insertion

--- react_interview/boilerplate/react_18th_v/src/App.js
+++ react_interview/boilerplate/react_18th_v/src/App.js
@@ -1,13 +1,29 @@
-import React, { memo, useCallback } from "react";
+import React, { memo, useCallback, useMemo } from "react";
 
 const App = () => {
   //state
   const [showNode, setShowNode] = React.useState(false);
   const [count, setCount] = React.useState(0);
+  const [numbers, setNumbers] = React.useState([11, 22, 3]);
   //methods
   const handleCount = useCallback(() => {
     setCount((prev) => prev + 1);
   }, []);
+
+  const getMax = useMemo(() => {
+    console.log("getMAx called");
+    return Math.max(...numbers);
+  }, [numbers]);
+
+  // const getMax = () => {
+  //   console.log("getMAx called");
+  //   return Math.max(...numbers);
+  // };
+
+  const handleInsert = () => {
+    const num = Math.round(Math.random() * 25);
+    setNumbers((prev) => [...prev, num]);
+  };
 
   return (
     <div>
@@ -15,6 +31,12 @@ const App = () => {
       <button onClick={() => setShowNode((prev) => !prev)}>
         {showNode ? "Hide" : "Show"} - Node
       </button>
+      <button onClick={handleInsert}>INSERT NUM</button>
+      {/* memo */}
+      <pre>{JSON.stringify(numbers, null, 2)}</pre>
+      {/* <h1>{getMax()}</h1> */}
+      <h1>{getMax}</h1>
+      {/* memo */}
       <br />
       <hr />
       <br />
