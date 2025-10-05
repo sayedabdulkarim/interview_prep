1fd64643884a297f187e787dd9e31d8a15603658
Sayed Abdul <sakarim9124@gmail.com>
2025-10-05 19:50:13 +0530

    Refactor App component to simplify state management and enhance child component interaction

--- react_interview/boilerplate/react_18th_v/src/App.js
+++ react_interview/boilerplate/react_18th_v/src/App.js
@@ -1,67 +1,38 @@
-import React, { memo, useCallback, useMemo } from "react";
+import React, { forwardRef, useImperativeHandle } from "react";
 
 const App = () => {
-  //state
-  const [showNode, setShowNode] = React.useState(false);
   const [count, setCount] = React.useState(0);
-  const [numbers, setNumbers] = React.useState([11, 22, 3]);
-  //methods
-  const handleCount = useCallback(() => {
-    setCount((prev) => prev + 1);
-  }, []);
+  const childRef = React.useRef();
 
-  const getMax = useMemo(() => {
-    console.log("getMAx called");
-    return Math.max(...numbers);
-  }, [numbers]);
-
-  // const getMax = () => {
-  //   console.log("getMAx called");
-  //   return Math.max(...numbers);
-  // };
-
-  const handleInsert = () => {
-    const num = Math.round(Math.random() * 25);
-    setNumbers((prev) => [...prev, num]);
+  const handleClick = () => {
+    if (childRef.current) {
+      console.log("clicked from child");
+      childRef.current.childClick();
+    }
   };
 
   return (
     <div>
-      <h1>Parent</h1>
-      <button onClick={() => setShowNode((prev) => !prev)}>
-        {showNode ? "Hide" : "Show"} - Node
-      </button>
-      <button onClick={handleInsert}>INSERT NUM</button>
-      {/* memo */}
-      <pre>{JSON.stringify(numbers, null, 2)}</pre>
-      {/* <h1>{getMax()}</h1> */}
-      <h1>{getMax}</h1>
-      {/* memo */}
-      <br />
-      <hr />
-      <br />
-      <ChildOne count={count} handleCount={handleCount} />
-      <br />
-      <hr />
-      <br />
-      <ChildTwo />
+      <h1>App Component</h1>
+      <ChildrenComp ref={childRef} />
+      <button onClick={handleClick}>Click Me</button>
     </div>
   );
 };
 
-const ChildOne = memo(({ count, handleCount }) => {
-  console.log(" cliked from childOne");
-  return (
-    <div>
-      <h1>ChildOne - {count}</h1>
-      <button onClick={handleCount}>SET COUNT</button>
-    </div>
+const ChildrenComp = forwardRef((props, ref) => {
+  useImperativeHandle(
+    ref,
+    () => {
+      return {
+        childClick() {
+          console.log("clicked from child");
+        },
+      };
+    },
+    []
   );
-});
-
-const ChildTwo = memo(() => {
-  console.log(" cliked from childTwo");
-  return <div>ChildTwo</div>;
+  return <div>children</div>;
 });
 
 export default App;
