d185bc7dd3d10eafe63dccd1674dcb8740974ee9
Sayed Abdul <sakarim9124@gmail.com>
2025-10-06 12:13:12 +0530

    Refactor index.js to remove custom useState implementation and streamline rendering process

--- react_interview/boilerplate/react_18th_v/src/index.js
+++ react_interview/boilerplate/react_18th_v/src/index.js
@@ -1,50 +1,18 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
 
-let root = null;
-const stateValues = [];
-let stateIndex = 0;
+//TODO: to verify
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  // <React.StrictMode>
+  <App />
+  // </React.StrictMode>
+);
 
-const useState = (val) => {
-  const currentIdx = stateIndex;
-  stateValues[currentIdx] = stateValues[currentIdx] ?? val;
-
-  const setVal = (val) => {
-    stateValues[currentIdx] = val;
-    renderApp();
-  };
-  stateIndex++;
-  return [stateValues[currentIdx], setVal];
-};
-
-const App = () => {
-  stateIndex = 0;
-
-  const [count1, setCount1] = useState(0);
-  const [count2, setCount2] = useState(0);
-
-  return (
-    <div>
-      <h1>{count1}</h1>
-      <button onClick={() => setCount1(count1 + 1)}>Add</button>
-      <button onClick={() => setCount1(count1 - 1)}>Minus</button>
-      <hr />
-      <h1>{count2}</h1>
-      <button onClick={() => setCount2(count2 + 1)}>Add</button>
-      <button onClick={() => setCount2(count2 - 1)}>Minus</button>
-    </div>
-  );
-};
-
-const renderApp = () => {
-  if (!root) {
-    root = ReactDOM.createRoot(document.getElementById("root"));
-  }
-  root.render(
-    <>
-      <App />
-    </>
-  );
-};
-
-renderApp();
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
