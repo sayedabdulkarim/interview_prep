e3b91d2a123d125746790e46724c180280ca9cb3
Sayed Abdul <sakarim9124@gmail.com>
2025-10-08 11:31:51 +0530

    Implement parent-child component interaction with ref and imperative handle

--- react_interview/boilerplate/react_18th_v/src/App.js
+++ react_interview/boilerplate/react_18th_v/src/App.js
@@ -1,7 +1,37 @@
 import React from "react";
 
 const App = () => {
-  return <div>App</div>;
+  const childRef = React.useRef();
+
+  const handleClick = () => {
+    if (childRef.current) {
+      childRef.current.childClicked();
+    }
+  };
+
+  return (
+    <div>
+      <h1>Parent Component</h1>
+      <ChildComponent ref={childRef} />
+      <button onClick={handleClick}>Call Child Method</button>
+    </div>
+  );
 };
+
+const ChildComponent = React.forwardRef((props, ref) => {
+  React.useImperativeHandle(
+    ref,
+    () => {
+      return {
+        childClicked: () => {
+          console.log("Child method called from Parent");
+        },
+      };
+    },
+    []
+  );
+
+  return <div>Child Component</div>;
+});
 
 export default App;
