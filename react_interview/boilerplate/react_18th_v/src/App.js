a0a81333dbb8b7dbcc0de805442183fd6b7dcd46
Sayed Abdul <sakarim9124@gmail.com>
2025-10-11 10:00:46 +0530

    Implement child component interaction in App.js with useRef and forwardRef

--- react_interview/boilerplate/react_18th_v/src/App.js
+++ react_interview/boilerplate/react_18th_v/src/App.js
@@ -1,7 +1,34 @@
-import React from "react";
+import React, { useImperativeHandle, forwardRef, useRef } from "react";
 
 const App = () => {
-  return <div>App</div>;
+  const childRef = useRef(null);
+
+  const handleClick = () => {
+    if (childRef.current) {
+      childRef.current.chlldClick();
+    }
+  };
+
+  return (
+    <div>
+      <h1>Parent Component</h1>
+      <ChildComp ref={childRef} />
+      <button onClick={handleClick}>Click Me</button>
+    </div>
+  );
 };
+
+// const ChildComp = useForwardRef((props, ref) => {
+const ChildComp = forwardRef((props, ref) => {
+  useImperativeHandle(ref, () => {
+    return {
+      chlldClick: () => {
+        console.log("child click");
+      },
+    };
+  });
+
+  return <div>ChildComp</div>;
+}, []);
 
 export default App;
