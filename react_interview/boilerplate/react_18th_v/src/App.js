702b2902a4bc522a26b584dc1746213852e1cd27
Sayed Abdul <sakarim9124@gmail.com>
2025-10-11 09:41:45 +0530

    Remove unused child component and related logic from App.js

--- react_interview/boilerplate/react_18th_v/src/App.js
+++ react_interview/boilerplate/react_18th_v/src/App.js
@@ -1,37 +1,7 @@
 import React from "react";
 
 const App = () => {
-  const childRef = React.useRef(null);
-
-  const handleClick = () => {
-    if (childRef.current) {
-      childRef.current.childClick();
-    }
-  };
-
-  return (
-    <div>
-      <h1>Parent Component</h1>
-      <ChildComp ref={childRef} />
-      <button onClick={handleClick}>Call Child Method</button>
-    </div>
-  );
+  return <div>App</div>;
 };
-
-const ChildComp = React.forwardRef((props, ref) => {
-  React.useImperativeHandle(
-    ref,
-    () => {
-      return {
-        childClick: () => {
-          console.log("Child component method called!");
-        },
-      };
-    },
-    []
-  );
-
-  return <div>Child Component</div>;
-});
 
 export default App;
