709d8de34bd1835cde427f32b32f5d8ebabd3761
Sayed Abdul <sakarim9124@gmail.com>
2025-10-13 08:14:11 +0530

    Refactor App.js to remove unused state and effect, simplifying input handling and child component rendering

--- react_interview/boilerplate/react_18th_v/src/App.js
+++ react_interview/boilerplate/react_18th_v/src/App.js
@@ -1,18 +1,57 @@
 import React from "react";
-import { useEffect } from "react";
+
+const populateArray = (n) => {
+  let arr = [];
+  for (let i = 1; i <= n; i++) {
+    arr.push(i);
+  }
+  return arr;
+};
+
+let data = populateArray(20000);
+
+function getFilteredData(val) {
+  if (!val) return data;
+  return data.filter((item) => item.toString().includes(val));
+}
 
 const App = () => {
-  const [count, setCount] = React.useState(0);
+  const [isPending, startTransition] = React.useTransition();
+  const [inputVal, setInputVal] = React.useState("");
+  const filteredData = React.useMemo(
+    () => getFilteredData(inputVal),
+    [inputVal]
+  );
 
-  useEffect(() => {
-    console.log("App Mounted");
-  });
+  const handleChange = (e) => {
+    startTransition(() => {
+      setInputVal(e.target.value);
+    });
+    // setInputVal(e.target.value);
+  };
+
   return (
     <div>
-      <h1>React 18th Edition</h1>
-      <h2>Count: {count}</h2>
-      <button onClick={() => setCount(count + 1)}>Increment</button>
-      <button onClick={() => setCount(count - 1)}>Decrement</button>
+      <input type="text" value={inputVal} onChange={handleChange} />
+      {isPending ? (
+        <h1 style={{ color: "white" }}>Loading...</h1>
+      ) : (
+        <ChildComponent data={filteredData} />
+      )}
+    </div>
+  );
+};
+
+const ChildComponent = ({ data }) => {
+  const defferedData = React.useDeferredValue(data);
+  return (
+    <div>
+      <ul>
+        {/* {data.map((item, index) => ( */}
+        {data.map((item, index) => (
+          <li key={index}>{item}</li>
+        ))}
+      </ul>
     </div>
   );
 };
