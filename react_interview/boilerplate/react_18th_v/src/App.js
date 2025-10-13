b05a93411fc73176de723374dcd03a1a1aca6361
Sayed Abdul <sakarim9124@gmail.com>
2025-10-13 08:20:11 +0530

    Refactor App.js to remove unused data handling and child component logic, simplifying the component structure

--- react_interview/boilerplate/react_18th_v/src/App.js
+++ react_interview/boilerplate/react_18th_v/src/App.js
@@ -1,59 +1,53 @@
 import React from "react";
 
-const populateArray = (n) => {
-  let arr = [];
-  for (let i = 1; i <= n; i++) {
-    arr.push(i);
+class ErrorBoundary extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { hasError: false };
   }
-  return arr;
-};
 
-let data = populateArray(20000);
+  static getDerivedStateFromError(error) {
+    // Update state so the next render will show the fallback UI.
+    return { hasError: true };
+  }
 
-function getFilteredData(val) {
-  if (!val) return data;
-  return data.filter((item) => item.toString().includes(val));
+  componentDidCatch(error, errorInfo) {
+    // You can also log the error to an error reporting service
+    console.log({ error, errorInfo }, " from componentDidCatch");
+  }
+
+  render() {
+    return (
+      <div>
+        {this.state.hasError ? (
+          <h1>Something went wrong.</h1>
+        ) : (
+          this.props.children
+        )}
+      </div>
+    );
+  }
 }
 
 const App = () => {
-  const [isPending, startTransition] = React.useTransition();
-  const [inputVal, setInputVal] = React.useState("");
-  const filteredData = React.useMemo(
-    () => getFilteredData(inputVal),
-    [inputVal]
-  );
-
-  const handleChange = (e) => {
-    startTransition(() => {
-      setInputVal(e.target.value);
-    });
-    // setInputVal(e.target.value);
-  };
-
   return (
     <div>
-      <input type="text" value={inputVal} onChange={handleChange} />
-      {isPending ? (
-        <h1 style={{ color: "white" }}>Loading...</h1>
-      ) : (
-        <ChildComponent data={filteredData} />
-      )}
+      <h1>Parent Component</h1>
+      <ErrorBoundary>
+        <ButtonComp />
+      </ErrorBoundary>
     </div>
   );
 };
 
-const ChildComponent = ({ data }) => {
-  const defferedData = React.useDeferredValue(data);
-  return (
-    <div>
-      <ul>
-        {/* {data.map((item, index) => ( */}
-        {data.map((item, index) => (
-          <li key={index}>{item}</li>
-        ))}
-      </ul>
-    </div>
-  );
+const ButtonComp = () => {
+  const [isBreaking, setIsBreaking] = React.useState(false);
+
+  if (isBreaking) {
+    throw new Error("I am broken");
+  }
+
+  return <button onClick={() => setIsBreaking(true)}>Break me</button>;
 };
 
 export default App;
