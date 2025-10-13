ef133b6edd9d6a59a65155192430265b9ae0e4f8
Sayed Abdul <sakarim9124@gmail.com>
2025-10-13 07:47:50 +0530

    Refactor App.js to remove timer functionality and simplify state management

--- react_interview/boilerplate/react_18th_v/src/App.js
+++ react_interview/boilerplate/react_18th_v/src/App.js
@@ -1,48 +1,18 @@
 import React from "react";
 import { useEffect } from "react";
-import { useState } from "react";
 
 const App = () => {
-  const [seconds, setSeconds] = useState(10);
-  const [minutes, setMinutes] = useState(0);
-  const [hours, setHours] = useState(0);
-  const [isStart, setIsStart] = useState(false);
-
-  const handleTimer = (sec, min, hour) => {
-    if (sec > 0) {
-      setSeconds((prev) => prev - 1);
-    } else if (sec === 0 && min >= 1) {
-      setSeconds(59);
-      setMinutes((prev) => prev - 1);
-    } else if (sec === 0 && min === 0 && hour === 0) {
-      setHours(0);
-      setMinutes(0);
-      setSeconds(0);
-    } else {
-      setHours((prev) => prev - 1);
-      setMinutes(59);
-      setSeconds(59);
-    }
-  };
+  const [count, setCount] = React.useState(0);
 
   useEffect(() => {
-    if (isStart) {
-      let startTimer = setInterval(() => {
-        handleTimer(seconds, minutes, hours);
-      }, 1000);
-      return () => clearInterval(startTimer);
-    }
-  }, [hours, isStart, minutes, seconds]);
-
+    console.log("App Mounted");
+  });
   return (
     <div>
-      <h1>Timer</h1>
-      <div>
-        {hours}:{minutes}:{seconds}
-      </div>
-      <button onClick={() => setIsStart(!isStart)}>
-        {isStart ? "Stop" : "Start"}
-      </button>
+      <h1>React 18th Edition</h1>
+      <h2>Count: {count}</h2>
+      <button onClick={() => setCount(count + 1)}>Increment</button>
+      <button onClick={() => setCount(count - 1)}>Decrement</button>
     </div>
   );
 };
