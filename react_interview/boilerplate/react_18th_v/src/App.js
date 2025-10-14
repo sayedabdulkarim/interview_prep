afb938a7699571ed6234225251d8aa1c8650b061
Sayed Abdul <sakarim9124@gmail.com>
2025-10-14 10:07:38 +0530

    Refactor App.js to remove ErrorBoundary and related components, simplifying the application structure

--- react_interview/boilerplate/react_18th_v/src/App.js
+++ react_interview/boilerplate/react_18th_v/src/App.js
@@ -1,49 +1,48 @@
 import React from "react";
-import { Component } from "react";
-import { useState } from "react";
-
-class ErrorBoundary extends Component {
-  constructor(props) {
-    super(props);
-    this.state = { hasError: false };
-  }
-
-  static getDerivedStateFromError(error) {
-    return { hasError: true };
-  }
-
-  componentDidCatch(error, info) {
-    console.error("Caught an error:", error, info);
-  }
-
-  render() {
-    return this.state.hasError ? (
-      <h1>Something went wrong.</h1>
-    ) : (
-      this.props.children
-    );
-  }
-}
+import { useEffect } from "react";
 
 const App = () => {
+  const [seconds, setSeconds] = React.useState(10);
+  const [minutes, setMinutes] = React.useState(1);
+  const [hours, setHours] = React.useState(0);
+  const [isStart, setIsStart] = React.useState(false);
+
+  const createTimer = (sec, min, hours) => {
+    if (sec > 0) {
+      setSeconds((prev) => prev - 1);
+    } else if (sec === 0 && min >= 1) {
+      setSeconds(59);
+      setMinutes((prev) => prev - 1);
+    } else if (sec === 0 && min === 0 && hours === 0) {
+      setHours(0);
+      setMinutes(0);
+      setSeconds(0);
+    } else {
+      setHours((prev) => prev - 1);
+      setMinutes(0);
+      setSeconds(0);
+    }
+  };
+
+  useEffect(() => {
+    let timer = setInterval(() => {
+      if (isStart) {
+        createTimer(seconds, minutes, hours);
+      }
+    }, 1000);
+
+    return () => clearInterval(timer);
+  }, [isStart, seconds, minutes, hours]);
+
   return (
-    <>
-      <h1>Parent Component</h1>
-      <ErrorBoundary>
-        <ButtonThatBreaks />
-      </ErrorBoundary>
-    </>
+    <div>
+      <h1>Hello World!</h1>
+      <strong>{`${hours}:${minutes}:${seconds}`}</strong>
+      <button onClick={() => setIsStart(!isStart)}>
+        {isStart ? "Stop" : "Start"}
+      </button>
+    </div>
   );
-};
-
-const ButtonThatBreaks = () => {
-  const [isBreak, setIsBreak] = useState(false);
-
-  if (isBreak) {
-    throw new Error("I am broken");
-  }
-
-  return <button onClick={() => setIsBreak(true)}>Break Me</button>;
 };
 
 export default App;
