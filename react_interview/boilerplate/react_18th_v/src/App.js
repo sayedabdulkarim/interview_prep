3837042b8fd241ed85a46cb2ad84a50d3b41946a
Sayed Abdul <sakarim9124@gmail.com>
2025-10-16 10:05:50 +0530

    updated

--- react_interview/boilerplate/react_18th_v/src/App.js
+++ react_interview/boilerplate/react_18th_v/src/App.js
@@ -1,44 +1,46 @@
 import React, { useEffect } from "react";
 
 const App = () => {
-  const [seconds, setSeconds] = React.useState(10);
-  const [minutes, setMinutes] = React.useState(1);
-  const [hours, setHours] = React.useState(0);
-  const [isStart, setIsStart] = React.useState(false);
+  return (
+    <div>
+      <h1>Parent</h1>
+      <Modal />
+    </div>
+  );
+};
 
-  const handleTimer = () => {
-    if (seconds > 0) {
-      setSeconds((prev) => prev - 1);
-    } else if (seconds === 0 && minutes >= 1) {
-      setSeconds(59);
-      setMinutes((prev) => prev - 1);
-    } else if (seconds === 0 && minutes === 0 && hours === 0) {
-      setHours(0);
-      setMinutes(0);
-      setSeconds(0);
-    } else {
-      setHours((prev) => prev - 1);
-      setMinutes(59);
-      setSeconds(59);
-    }
+const Modal = () => {
+  const modalRef = React.useRef(null);
+  const [isShow, setIsShow] = React.useState(false);
+
+  const handleClose = () => {
+    setIsShow(false);
   };
 
   useEffect(() => {
-    if (isStart) {
-      let timer = setInterval(() => {
-        handleTimer();
-      }, 1000);
-      return () => clearInterval(timer);
-    }
-  }, [isStart, seconds, minutes, hours]);
+    const handleClickOutside = (e) => {
+      if (modalRef.current && !modalRef.current.contains(e.target)) {
+        handleClose();
+      }
+    };
+    document.addEventListener("mousedown", handleClickOutside);
+
+    return () => {
+      document.removeEventListener("mousedown", handleClickOutside);
+    };
+  }, []);
 
   return (
     <div>
-      <h1>App Component</h1>
-      <p>{`${hours}:${minutes}:${seconds}`}</p>
-      <button onClick={() => setIsStart(!isStart)}>
-        {isStart ? "Pause" : "Start"}
-      </button>
+      <h1>Modal</h1>
+      <button onClick={() => setIsShow(true)}>Open Modal</button>
+
+      {isShow && (
+        <div style={{ border: "1px solid red" }} ref={modalRef}>
+          {<p>This is modal content</p>}
+          <button onClick={handleClose}>Close</button>
+        </div>
+      )}
     </div>
   );
 };
