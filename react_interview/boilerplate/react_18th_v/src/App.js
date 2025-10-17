4b4e9a97d04caeba45c8e97dbf0d05ed670cfebe
Sayed Abdul <sakarim9124@gmail.com>
2025-10-17 09:19:49 +0530

    updated

--- react_interview/boilerplate/react_18th_v/src/App.js
+++ react_interview/boilerplate/react_18th_v/src/App.js
@@ -1,49 +1,48 @@
 import React from "react";
-import { useCallback } from "react";
+import { useRef } from "react";
 import { useEffect } from "react";
+import { useState } from "react";
 
 const App = () => {
-  const [second, setSecond] = React.useState(10);
-  const [minute, setMinute] = React.useState(1);
-  const [hour, setHour] = React.useState(0);
-  const [isStart, setIsStart] = React.useState(false);
+  return (
+    <div>
+      <h1>Parent Component</h1>
+      <ModalComponent />
+    </div>
+  );
+};
 
-  const handleTimer = useCallback(() => {
-    if (second > 0) {
-      setSecond((prev) => prev - 1);
-    } else if (second === 0 && minute > 0) {
-      setSecond(59);
-      setMinute((prev) => prev - 1);
-    } else if (second === 0 && minute === 0 && hour === 0) {
-      setSecond(0);
-      setMinute(0);
-      setHour(0);
-    } else if (second === 0 && minute === 0 && hour > 0) {
-      setSecond(59);
-      setMinute(59);
-      setHour((prev) => prev - 1);
-    }
-  }, [second, minute, hour]);
+const ModalComponent = () => {
+  const modalRef = useRef();
+  const [isShow, setIsShow] = useState(false);
+
+  const handleClose = () => {
+    setIsShow(false);
+  };
 
   useEffect(() => {
-    if (isStart) {
-      const timer = setInterval(() => {
-        handleTimer();
-      }, 1000);
+    const handleClickOutside = (e) => {
+      if (modalRef.current && modalRef.current.contains(e.target) === false) {
+        handleClose();
+      }
+    };
 
-      return () => clearInterval(timer);
-    }
-  }, [isStart, second, minute, hour, handleTimer]);
+    document.addEventListener("mousedown", handleClickOutside);
+    return () => {
+      document.removeEventListener("mousedown", handleClickOutside);
+    };
+  }, []);
 
   return (
     <div>
-      <h1>Timer</h1>
-      <div>
-        {hour}:{minute}:{second}
-      </div>
-      <button onClick={() => setIsStart(!isStart)}>
-        {isStart ? "Stop" : "Start"}
-      </button>
+      <button onClick={() => setIsShow(true)}>Show Modal</button>
+      {isShow && (
+        <div ref={modalRef} style={{ border: "2px solid red" }}>
+          <h1>Modal Component</h1>
+          <p>This is modal content</p>
+          <button onClick={handleClose}>Close Modal</button>
+        </div>
+      )}
     </div>
   );
 };
