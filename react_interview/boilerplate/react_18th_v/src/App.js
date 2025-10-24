c06b95d3da824cb84d22239547a840a4ec8bd810
Sayed Abdul <sakarim9124@gmail.com>
2025-10-24 22:02:00 +0530

    updated

--- react_interview/boilerplate/react_18th_v/src/App.js
+++ react_interview/boilerplate/react_18th_v/src/App.js
@@ -1,10 +1,13 @@
-import React, { useCallback } from "react";
+import React, { useCallback, useMemo } from "react";
 // import ChildOne from "./components/test/ChildOne";
 // import ChildTwo from "./components/test/ChildTwo";
 
 const App = () => {
   const [count, setCount] = React.useState(0);
   const [isShown, setIsShown] = React.useState(true);
+  const [numbers, setNumbers] = React.useState([
+    49, 65, 3, 12, 117, 89, 23, 45, 68, 90,
+  ]); // 10 random numbers
 
   const handleToggle = () => {
     setIsShown((prev) => !prev);
@@ -14,11 +17,29 @@ const App = () => {
     setCount((prev) => prev + 1);
   }, []);
 
+  //   const getMaxNumber = useCallback(() => {
+  //     console.log("getMaxNumber called");
+  //     return Math.max(...numbers);
+  //   }, []);
+  const getMaxNumber = useMemo(() => {
+    console.log("getMaxNumber called");
+    return Math.max(...numbers);
+  }, [numbers]);
+
   console.log("Parent Rendered");
   return (
     <div>
       <h1>Parent App {isShown ? "shown" : "hidden"}</h1>
       <button onClick={handleToggle}>Toggle Child</button>
+      <hr />
+      <hr />
+      {/* <h2>Max Number: {getMaxNumber()}</h2> */}
+      <h2>Max Number: {getMaxNumber}</h2>
+      <button
+        onClick={() => setNumbers((prev) => [...prev, Math.random() * 500])}
+      >
+        Add Number
+      </button>
       <hr />
       <hr />
       <ChildOne count={count} handleCount={handleCount} />
