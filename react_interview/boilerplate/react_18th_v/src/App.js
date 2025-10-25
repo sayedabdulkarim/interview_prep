d8ceb1692e914662d72a0df8694a36da00aab7ed
Sayed Abdul <sakarim9124@gmail.com>
2025-10-25 13:11:16 +0530

    updated

--- react_interview/boilerplate/react_18th_v/src/App.js
+++ react_interview/boilerplate/react_18th_v/src/App.js
@@ -1,31 +1,48 @@
-import React, { forwardRef, useImperativeHandle } from "react";
+import React, { useState } from "react";
+
+const getProd = (num) => {
+  const arr = [];
+  for (let i = 1; i <= num; i++) {
+    arr.push(i);
+  }
+  return arr;
+};
+
+const prod = getProd(10000);
+
+const filterProd = (arg) => prod.filter((i) => i.toString().includes(arg));
 
 const App = () => {
-  const childRef = React.useRef();
+  const [isPending, startTransition] = React.useTransition();
+  const [inp, setInp] = useState("");
+  const myProd = filterProd(inp);
 
-  const handleClick = () => {
-    if (childRef.current) {
-      childRef.current.childClick();
-    }
+  const handleChange = (e) => {
+    startTransition(() => {
+      setInp(e.target.value);
+    });
+    // setInp(e.target.value);
   };
 
   return (
     <div>
-      <h1>Parent Component</h1>
-      <button onClick={handleClick}>Call Child Method</button>
-      <ChildComponent ref={childRef} />
+      <input type="text" value={inp} onChange={handleChange} autoFocus />
+      {isPending ? <h2>Loading...</h2> : <ChildComponent data={myProd} />}
     </div>
   );
 };
 
-const ChildComponent = forwardRef((props, ref) => {
-  useImperativeHandle(ref, () => {
-    return {
-      childClick: () => {
-        console.log("Child component method called");
-      },
-    };
-  });
-  return <div>Child Component</div>;
-});
+const ChildComponent = ({ data }) => {
+  const deferredData = React.useDeferredValue(data);
+  return (
+    <div>
+      <ul>
+        {deferredData.map((item, index) => (
+          <li key={index}>{item}</li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
 export default App;
