a59998098b9b4838ab31e51766d7ada1882a48a4
Sayed Abdul <sakarim9124@gmail.com>
2025-11-16 19:39:21 +0530

    updated

--- react_interview/boilerplate/react_18th_v/src/App.js
+++ react_interview/boilerplate/react_18th_v/src/App.js
@@ -1,37 +1,19 @@
 import React from "react";
 
-// Component
-const ButtonGroup = ({ children }) => {
-  const buttonStyle = {
-    border: "2px solid red",
-    padding: "10px 20px",
-    margin: "5px",
-    backgroundColor: "#f0f0f0",
-    cursor: "pointer",
-    color: "green",
-  };
-
-  const styledChildren = React.Children.map(children, (child) => {
-    return React.cloneElement(child, {
-      style: { ...buttonStyle, ...child.props.style },
-    });
-  });
-
-  return <div>{styledChildren}</div>;
-};
-
-// Usage
-function App() {
+const App = () => {
   return (
     <div>
-      <ButtonGroup>
-        <button>Save</button>
-        <button>Close</button>
-        <button>Delete</button>
-        <h1>Hello</h1>
-      </ButtonGroup>
+      <div onClick={() => console.log("Outer")}>
+        <button onClick={() => console.log("Inner")}>Click</button>
+      </div>
+      <hr />
+      <hr />
+      <hr />
+      <div onClickCapture={() => console.log("Outer")}>
+        <button onClick={() => console.log("Inner")}>Click</button>
+      </div>
     </div>
   );
-}
+};
 
 export default App;
