012475d37d478a41228538026d2fbc31f5d61843
Sayed Abdul <sakarim9124@gmail.com>
2026-01-23 14:39:18 +0530

    updated

--- react_interview/boilerplate/react_18th_v/src/App.js
+++ react_interview/boilerplate/react_18th_v/src/App.js
@@ -1,19 +1,7 @@
 import React from "react";
 
 const App = () => {
-  return (
-    <div>
-      <div onClick={() => console.log("Outer")}>
-        <button onClick={() => console.log("Inner")}>Click</button>
-      </div>
-      <hr />
-      <hr />
-      <hr />
-      <div onClickCapture={() => console.log("Outer")}>
-        <button onClick={() => console.log("Inner")}>Click</button>
-      </div>
-    </div>
-  );
+  return <div>App</div>;
 };
 
 export default App;
